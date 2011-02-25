05e7117b5844d2a98d217eceb480708779c7148d
Ryan Bigg <radarlistener@gmail.com>
2011-02-26 10:00:01 +1100

    No need to manually load translations anymore

--- lib/forem/engine.rb
+++ lib/forem/engine.rb
@@ -11,6 +11,3 @@ require 'simple_form'
 #        incompatible encoding regexp match (UTF-8 regexp with ASCII-8BIT string)
 # Which happens when running integration specs containing a form
 require 'rack/utils_monkey_patch'
-
-# To load the translations, which *should* be automatically loaded
-I18n.load_path << File.expand_path("config/locales/en.yml", File.dirname(__FILE__) + "../../..")
