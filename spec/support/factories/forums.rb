5f17842b714f00937cea506b74a6c849400e2354
Ryan Bigg <radarlistener@gmail.com>
2011-03-03 11:53:22 +1100

    Add Factory Girl factories

---
+++ spec/support/factories/forums.rb
@@ -0,0 +1,4 @@
+# Factory.define(:forum) do |f|
+#   f.title "Welcome to Forem!"
+#   f.description "This is the first forum"
+# end
---
+++ spec/support/factories/topics.rb
@@ -0,0 +1,4 @@
+# Factory.define(:topic) do |t|
+#   t.subject "FIRST POST"
+#   # t.posts { |p| [p.association(:post)] }
+# end
