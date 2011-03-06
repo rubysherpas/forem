2b9a9c598cdda1b6c671d3e026a6c9036d9a989f
Adam McDonald <mcdonald.adam@gmail.com>
2011-03-05 20:05:36 -0800

    Cleaned up factories

--- spec/support/factories/posts.rb
+++ spec/support/factories/posts.rb
@@ -1,7 +1,4 @@
 Factory.define(:post, :class => Forem::Post) do |t|
   t.text "This is a brand new post!"
   t.user {|u| u.association(:user) }
-  
-  # t.assocation :topic
-  # t.assocation :user
 end
--- spec/support/factories/topics.rb
+++ spec/support/factories/topics.rb
@@ -1,9 +1,6 @@
 Factory.define(:topic, :class => Forem::Topic) do |t|
   t.subject "FIRST TOPIC"
-  #t.association :forum
   t.forum {|f| f.association(:forum) }
-  #t.posts { |p| [p.association(:post)] }
-  #t.assocation :user
   t.user {|u| u.association(:user) }
   t.posts { |p| [p.association(:post)]}
 end
