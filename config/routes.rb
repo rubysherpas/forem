8f005e31fc63d11c261756621f7fab395ec1717f
Ryan Bigg <radarlistener@gmail.com>
2011-03-10 11:45:49 +1100

    Use Rails 3.0 engine routing hack

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
-Forem::Engine.routes.draw do
-  root :to => "forums#index"
-  resources :forums do
-    resources :topics
-  end
+Rails.application.routes.draw do
+  namespace :forem do
+    root :to => "forums#index"
+    resources :forums do
+      resources :topics
+    end
 
-  resources :topics do
-    resources :posts
+    resources :topics do
+      resources :posts
+    end
   end
 end
