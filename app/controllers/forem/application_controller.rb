471cf3e4f228a667c478f1d91a30f4b6387027ba
Ryan Bigg <radarlistener@gmail.com>
2011-03-10 11:54:20 +1100

    Use namespaced route

--- app/controllers/forem/application_controller.rb
+++ app/controllers/forem/application_controller.rb
@@ -10,7 +10,7 @@ class Forem::ApplicationController < ApplicationController
   def authenticate_forem_admin
     if !current_user || !current_user.forem_admin
       flash[:error] = t("forem.errors.access_denied")
-      redirect_to forums_path #TODO: not positive where to redirect here
+      redirect_to forem_forums_path #TODO: not positive where to redirect here
     end
   end
 
