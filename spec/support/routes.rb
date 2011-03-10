af469f93cadfa307df0ff06ab3c9e7e2a1eb4958
Ryan Bigg <radarlistener@gmail.com>
2011-03-10 11:47:16 +1100

    routes inclusion is no longer needed as routes are defined on app

--- spec/support/routes.rb
+++
@@ -1,5 +0,0 @@
-# This will include the routing helpers in the specs so that we can use
-# forum_path, forum_topic_path and so on to get to the routes.
-RSpec.configure do |c|
-  c.include Forem::Engine.routes.url_helpers
-end
