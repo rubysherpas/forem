8d0ae917864c49b0c8b63f17cf1a78622c7558c2
Ryan Bigg <radarlistener@gmail.com>
2011-03-10 13:42:00 +1100

    Defining this class using two lines makes spec fail. Revert back to class Forem::Application definition

--- app/controllers/forem/application_controller.rb
+++ app/controllers/forem/application_controller.rb
@@ -1,22 +1,20 @@
-module Forem
-  class ApplicationController < ApplicationController
-    def authenticate_forem_user
-      if !current_user
-        session[:return_to] = request.fullpath
-        flash[:error] = t("forem.errors.not_signed_in")
-        redirect_to "/sign_in" #TODO: Change to routing helper for flexibility
-      end
+class Forem::ApplicationController < ApplicationController
+  def authenticate_forem_user
+    if !current_user
+      session[:return_to] = request.fullpath
+      flash[:error] = t("forem.errors.not_signed_in")
+      redirect_to "/sign_in" #TODO: Change to routing helper for flexibility
     end
-  
-    def authenticate_forem_admin
-      if !current_user || !current_user.forem_admin
-        flash[:error] = t("forem.errors.access_denied")
-        redirect_to forums_path #TODO: not positive where to redirect here
-      end
-    end
+  end
 
-    # dummy method
-    def current_user
+  def authenticate_forem_admin
+    if !current_user || !current_user.forem_admin
+      flash[:error] = t("forem.errors.access_denied")
+      redirect_to forums_path #TODO: not positive where to redirect here
     end
+  end
+
+  # dummy method
+  def current_user
   end
 end
