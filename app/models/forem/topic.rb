b4110be76003fcff9be2f19d0c7ee240c01d9904
Josh Adams <josh@isotope11.com>
2011-05-20 10:19:10 -0500

    [add views] Add view model, register a view each topic show, and show view count on topics listing.

--- app/models/forem/topic.rb
+++ app/models/forem/topic.rb
@@ -2,7 +2,8 @@ module Forem
   class Topic < ActiveRecord::Base
     belongs_to :forum
     belongs_to :user
-    has_many :posts, :dependent => :destroy
+    has_many   :views
+    has_many   :posts, :dependent => :destroy
     accepts_nested_attributes_for :posts
 
     validates :subject, :presence => true
---
+++ db/migrate/20110520150056_add_views.rb
@@ -0,0 +1,8 @@
+class AddViews < ActiveRecord::Migration
+  def change
+    create_table :forem_views do |t|
+      t.integer :user_id
+      t.integer :topic_id
+    end
+  end
+end
