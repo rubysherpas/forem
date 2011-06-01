cbb8f205f808a8e3566de82b522f1855295dde49
Ryan Bigg <radarlistener@gmail.com>
2011-06-01 21:44:03 +1000

    Add basic theming support to forem. See this post for an explanation: http://ryanbigg.com/2011/06/a-potential-use-for-asset-pipelining/

--- app/helpers/forem/application_helper.rb
+++ app/helpers/forem/application_helper.rb
@@ -4,5 +4,9 @@ module Forem
       text = sanitize(text) unless text.html_safe? || options.delete(:safe)
       (text.blank? ? "" : RDiscount.new(text).to_html).html_safe
     end
+    
+    def forem_theme_tag
+      stylesheet_link_tag("forem/#{Forem::Engine.theme}/style.css")
+    end
   end
 end
