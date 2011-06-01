9869bc61accbd4a301f3f53d51da1cf2d2a9be58
Ryan Bigg <radarlistener@gmail.com>
2011-06-01 21:01:57 +1000

    Remove Rack monkey patch which is no longer needed

--- lib/forem/engine.rb
+++ lib/forem/engine.rb
@@ -37,9 +37,3 @@ module Forem
 end
 
 require 'simple_form'
-
-# Fixes this error:
-# Encoding::CompatibilityError:
-#        incompatible encoding regexp match (UTF-8 regexp with ASCII-8BIT string)
-# Which happens when running integration specs containing a form
-require 'rack/utils_monkey_patch'
