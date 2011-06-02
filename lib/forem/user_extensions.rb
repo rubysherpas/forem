f16aaaf67b9a85d821edeb53582a9e7b0fbad478
Josh Adams <josh@isotope11.com>
2011-06-02 15:00:49 -0500

    [refinery] Make refinery user's forem_admin? key off of refinery roles.

--- lib/forem/user_extensions.rb
+++ lib/forem/user_extensions.rb
@@ -3,6 +3,13 @@ module Forem
     def self.included(base)
       Forem::Topic.belongs_to :user, :class_name => base.to_s 
       Forem::Post.belongs_to :user, :class_name => base.to_s
+
+      # FIXME: Obviously terrible, temp to get further
+      def forem_admin?
+        %w(refinery superuser).detect do |r|
+          has_role?(r)
+        end
+      end
     end
   end
   
