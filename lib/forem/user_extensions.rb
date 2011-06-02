c9ff09ce9ab435a7d9a1449e803f741d7fd3fe51
Ryan Bigg <radarlistener@gmail.com>
2011-06-02 11:48:26 +1000

    Forem::User -> Forem::Post

--- lib/forem/user_extensions.rb
+++ lib/forem/user_extensions.rb
@@ -2,7 +2,7 @@ module Forem
   module UserExtensions
     def self.included(base)
       Forem::Topic.belongs_to :user, :class_name => base.to_s 
-      Forem::User.belongs_to :user, :class_name => base.to_s
+      Forem::Post.belongs_to :user, :class_name => base.to_s
     end
   end
   
