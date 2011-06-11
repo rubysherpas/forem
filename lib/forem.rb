a404b8e729e0d70b37dcbfb3eecd8a47ae01aacc
Ryan Bigg <radarlistener@gmail.com>
2011-06-12 09:52:54 +1000

    Forem::Engine.theme is now Forem.theme

--- lib/forem.rb
+++ lib/forem.rb
@@ -1,10 +1,9 @@
 require "forem/engine"
-require "forem/kaminari_config"
 require 'rdiscount'
 
 module Forem
 
-  mattr_accessor :user_class
+  mattr_accessor :user_class, :theme
 
   class << self
     def user_class
@@ -16,7 +15,8 @@ module Forem
     attr_accessor :message
     def initialize(option)
       @message = "Forem configuration option #{option} not found. " + 
-        "Please set this in config/initializers/forem.rb."
+        "Please set this in config/initializers/forem.rb with this line:\n\n" +
+        "Forem.#{option}= <value>"
     end
   end
 
