3f256c5165932c598b2414f7d775c606cedf8d4b
Josh Adams <josh@isotope11.com>
2011-06-25 11:30:10 -0500

    [refinery] remove refinery plugin initializer.  to be handled in a refinery engine.

--- lib/forem/engine.rb
+++ lib/forem/engine.rb
@@ -8,18 +8,6 @@ module ::Forem
         @root ||= Pathname.new(File.expand_path('../../', __FILE__))
       end
     end
-
-    config.after_initialize do
-      if defined?(::Refinery)
-        # Allow forem to hook into Refinery CMS if it's available
-        ::Refinery::Plugin.register do |plugin|
-          plugin.name = "refinery_forem"
-          plugin.directory = "forem"
-          plugin.url = '/forem/admin/forums'
-          plugin.menu_match = /^\/?(admin|refinery)\/forem\/?(forums|posts|topics)?/
-        end
-      end
-    end
   end
 end
 
