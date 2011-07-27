809784b4de219d0aa911f3a6d209c3705742904d
Philip Arndt <parndt@gmail.com>
2011-07-28 11:58:19 +1200

    Added gravatar abilities without the need for a separate gem. Fixes #15

--- app/helpers/forem/posts_helper.rb
+++ app/helpers/forem/posts_helper.rb
@@ -1,8 +1,16 @@
 module Forem
   module PostsHelper
-    def avatar(user='')
-      email = user.try(:email)
-      gravatar_image_tag(email, :gravatar => { :default => :identicon }).html_safe if email.present?
+    def avatar(user, options = {})
+      if (email = user.try(:email)).present?
+        avatar_url(email, options).html_safe
+      end
+    end
+
+    def avatar_url(email, options = {})
+      options = {:size => 60}
+      require 'digest/md5'
+      size = ("?s=#{options[:size]}" if options[:size])
+      "http://gravatar.com/avatar/#{Digest::MD5.hexdigest(email.to_s.strip.downcase)}#{size}.jpg"
     end
   end
 end
--- lib/forem.rb
+++ lib/forem.rb
@@ -1,7 +1,6 @@
 require "forem/engine"
 require 'rdiscount'
 require 'kaminari'
-require 'gravatar_image_tag'
 
 module Forem
 
@@ -12,11 +11,11 @@ module Forem
       @@user_class || raise(ConfigurationNotFound.new("user_class"))
     end
   end
-  
+
   class ConfigurationNotFound < StandardError
     attr_accessor :message
     def initialize(option)
-      @message = "Forem configuration option #{option} not found. " + 
+      @message = "Forem configuration option #{option} not found. " +
         "Please set this in config/initializers/forem.rb with this line:\n\n" +
         "Forem.#{option}= <value>"
     end
