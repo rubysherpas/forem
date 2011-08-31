b1ffd4fb9174992a590220056bec5638c000eabb
Philip Arndt <parndt@gmail.com>
2011-09-01 00:34:54 +1200

    removed duplicate migration

--- spec/dummy/db/migrate/20110626160056_add_hidden_to_topics.rb
+++
@@ -1,5 +0,0 @@
-class AddHiddenToTopics < ActiveRecord::Migration
-  def change
-    add_column :forem_topics, :hidden, :boolean, :default => false
-  end
-end
--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -1,3 +1,4 @@
+# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -10,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110708030915) do
+ActiveRecord::Schema.define(:version => 20110828132917) do
 
   create_table "forem_forums", :force => true do |t|
     t.string "title"
