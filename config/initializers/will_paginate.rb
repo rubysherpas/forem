e08c66ce2fa5b661a37e546b366b367bfb217a51
Ryan Bigg <radarlistener@gmail.com>
2011-09-10 10:07:34 +1000

    Potential fix for #38. Add workaround for pagination when will_paginate is loaded. Forem will continue to 'pretend' to use Kaminari helpers but will fall back to will_paginate when needed

---
+++ config/initializers/will_paginate.rb
@@ -0,0 +1,12 @@
+Rails.application.config.after_initialize do
+  if defined?(WillPaginate)
+    module WillPaginate
+      module ActiveRecord
+        module RelationMethods
+          alias_method :per, :per_page
+          alias_method :num_pages, :total_pages
+        end
+      end
+    end
+  end
+end
