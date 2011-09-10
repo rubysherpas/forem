c47b228789af2fbc65d176ec8c83be6df1af4b6a
Ryan Bigg <radarlistener@gmail.com>
2011-09-10 10:07:34 +1000

    Potential fix for #38. Add workaround for pagination when will_paginate is loaded. Forem will continue to 'pretend' to use Kaminari helpers but will fall back to will_paginate when needed

--- app/helpers/forem/application_helper.rb
+++ app/helpers/forem/application_helper.rb
@@ -8,5 +8,15 @@ module Forem
     def forem_theme_tag
       stylesheet_link_tag("forem/#{Forem.theme}/style.css")
     end
+
+    def forem_paginate(collection, options={})
+      if respond_to?(:will_paginate)
+        # If parent app is using Will Paginate, we need to use it also
+        will_paginate collection, options
+      else
+        # Otherwise use Kaminari
+        paginate collection, options
+      end
+    end
   end
 end
