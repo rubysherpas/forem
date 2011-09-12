f9c4b6637aca0309fce69b7753532af8106ce99f
Frankel Ma <frankelma@gmail.com>
2011-09-12 10:46:57 +0800

    Make some words i18n supported.

--- app/helpers/forem/topics_helper.rb
+++ app/helpers/forem/topics_helper.rb
@@ -1,7 +1,7 @@
 module Forem
   module TopicsHelper
     def link_to_latest_post(post)
-      text = "#{time_ago_in_words(post.created_at)} ago by #{post.user}"
+      text = "#{time_ago_in_words(post.created_at)} " + t("ago_by") + " #{post.user}"
       link_to text, forum_topic_path(post.topic.forum, post.topic, :anchor => "post-#{post.id}")
     end
   end
