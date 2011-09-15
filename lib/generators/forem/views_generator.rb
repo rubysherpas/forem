1b3b89a9b3adc5054e6ca7389e48aa5b0ed1fce5
Ryan Bigg <radarlistener@gmail.com>
2011-09-15 15:28:07 -0700

    Add forem:views generator, which copies over the forem views into your application

---
+++ lib/generators/forem/views_generator.rb
@@ -0,0 +1,24 @@
+# Thanks to plataformatec/devise
+# The code used to inspire this generator!
+require 'rails/generators'
+module Forem
+  module Generators
+    class ViewsGenerator < Rails::Generators::Base #:nodoc:
+      source_root File.expand_path("../../../../app/views/forem", __FILE__)
+      desc "Used to copy forem's views to your application's views."
+
+      def copy_views
+        view_directory :admin
+        view_directory :forums
+        view_directory :posts
+        view_directory :topics
+      end
+
+      protected
+
+      def view_directory(name)
+        directory name.to_s, "app/views/forem/#{name}"
+      end
+    end
+  end
+end
