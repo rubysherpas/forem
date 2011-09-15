816e6b1f6c0315ec349e5483e5f3d6f0d4339960
Ryan Bigg <radarlistener@gmail.com>
2011-09-15 13:07:44 -0700

    Cleanup post_spec

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -3,9 +3,8 @@ require 'spec_helper'
 describe Forem::Post do
   let(:post) { FactoryGirl.create(:post) }
   let(:reply) { FactoryGirl.create(:post, :reply_to => post) }
-  
-  context "upon deletion" do
 
+  context "upon deletion" do
     it "clears the reply_to_id for all replies" do
       reply.reply_to.should eql(post)
       post.destroy
