156c3f629a8284cbfee420887f089034c272d870
Ryan Bigg <radarlistener@gmail.com>
2011-10-27 20:32:20 +1100

    Use simple_format as default 'formatter' and test that we can use forem-rdiscount to format different

--- app/helpers/forem/formatting_helper.rb
+++ app/helpers/forem/formatting_helper.rb
@@ -2,7 +2,11 @@ module Forem
   module FormattingHelper
     # override with desired markup formatter, e.g. textile or markdown
     def as_formatted_html(text)
-      Forem.formatter.format(text)
+      if Forem.formatter
+        Forem.formatter.format(text)
+      else
+        simple_format(h(text))
+      end
     end
   end
 end
