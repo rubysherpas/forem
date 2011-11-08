21b7fdc70795ea3bee79e5885fe9d981d69ff0c1
Gabriel Baez <gabrielbaez@bohiti.org>
2011-11-07 22:15:04 -0600

    Update Factories to support new categories functionality. #66 #74

---
+++ spec/dummy/db/migrate/20111108032749_add_category_id_to_forums.rb
@@ -0,0 +1,9 @@
+class AddCategoryIdToForums < ActiveRecord::Migration
+  def change
+    add_column :forem_forums, :category_id, :integer
+
+    if Forem::Forum.count > 0
+      Forem::Forum.update_all :category_id => Forem::Category.first.id
+    end
+  end
+end
--- spec/support/factories/forums.rb
+++ spec/support/factories/forums.rb
@@ -2,5 +2,6 @@ FactoryGirl.define do
   factory :forum, :class => Forem::Forum do |f|
     f.title "Welcome to Forem!"
     f.description "A placeholder forum."
+    f.category {|t| t.association(:category) }
   end
 end
