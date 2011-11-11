3feb5f0c845c80b86b09425f4ce4a73942c69708
Ryan Bigg <radarlistener@gmail.com>
2011-11-11 22:16:28 +1100

    WIP: Beginnings of basic permissions system using CanCan

--- spec/integration/forums_spec.rb
+++ spec/integration/forums_spec.rb
@@ -17,41 +17,56 @@ describe "forums" do
       FactoryGirl.create(:post, :topic => @topic_2, :created_at => Time.now + 30.seconds)
       @topic_3 = FactoryGirl.create(:topic, :subject => "PINNED!", :forum => forum, :pinned => true)
       @topic_4 = FactoryGirl.create(:topic, :subject => "HIDDEN!", :forum => forum, :hidden => true)
-      visit forum_path(forum.id)
     end
 
-    it "shows the title" do
-      within("#forum h2") do
-        page.should have_content("Welcome to Forem!")
-      end
-      within("#forum small") do
-        page.should have_content("A placeholder forum.")
-      end
-    end
-
-    it "lists pinned topics first" do
-      # TODO: cleaner way to get at topic subjects on the page?
-      topic_subjects = Nokogiri::HTML(page.body).css(".topics tbody tr .subject").map(&:text)
-      topic_subjects.should == ["PINNED!", "Most Recent", "Unpinned"]
-    end
-
-    it "does not show hidden topics" do
-      # TODO: cleaner way to get at topic subjects on the page?
-      topic_subjects = Nokogiri::HTML(page.body).css(".topics tbody tr .subject").map(&:text)
-      topic_subjects.include?("HIDDEN!").should be_false
-    end
-
-    context "when logged in" do
+    context "without ability to read forums" do
       before do
-        user = Factory(:user)
-        sign_in(user)
+        User.any_instance.stub(:can_read_forums?).and_return(false)
       end
-      it "calls out topics that have been posted to since your last visit, if you've visited" do
-        visit forum_topic_path(forum.id, @topic_2)
-        ::Forem::View.last.update_attribute(:updated_at, 1.minute.ago)
-        visit forum_path(forum)
-        topic_subjects = Nokogiri::HTML(page.body).css(".topics tbody tr .new_posts")
-        topic_subjects.should_not be_empty
+
+      it "is denied access" do
+        lambda { visit forum_path(forum.id) }.should raise_error(CanCan::AccessDenied)
+      end
+    end
+
+    context "with default permissions" do
+      before do
+        visit forum_path(forum.id)
+      end
+
+      it "shows the title" do
+        within("#forum h2") do
+          page.should have_content("Welcome to Forem!")
+        end
+        within("#forum small") do
+          page.should have_content("A placeholder forum.")
+        end
+      end
+
+      it "lists pinned topics first" do
+        # TODO: cleaner way to get at topic subjects on the page?
+        topic_subjects = Nokogiri::HTML(page.body).css(".topics tbody tr .subject").map(&:text)
+        topic_subjects.should == ["PINNED!", "Most Recent", "Unpinned"]
+      end
+
+      it "does not show hidden topics" do
+        # TODO: cleaner way to get at topic subjects on the page?
+        topic_subjects = Nokogiri::HTML(page.body).css(".topics tbody tr .subject").map(&:text)
+        topic_subjects.include?("HIDDEN!").should be_false
+      end
+
+      context "when logged in" do
+        before do
+          user = Factory(:user)
+          sign_in(user)
+        end
+        it "calls out topics that have been posted to since your last visit, if you've visited" do
+          visit forum_topic_path(forum.id, @topic_2)
+          ::Forem::View.last.update_attribute(:updated_at, 1.minute.ago)
+          visit forum_path(forum)
+          topic_subjects = Nokogiri::HTML(page.body).css(".topics tbody tr .new_posts")
+          topic_subjects.should_not be_empty
+        end
       end
     end
   end
