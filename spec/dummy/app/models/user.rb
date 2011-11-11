234379126defb805de9b03e7dab2c2a882bc8a17
Ryan Bigg <radarlistener@gmail.com>
2011-11-11 22:16:28 +1100

    WIP: Beginnings of basic permissions system using CanCan

--- spec/dummy/app/models/user.rb
+++ spec/dummy/app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  include Forem::DefaultPermissions
   # Include default devise modules. Others available are:
   # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable
