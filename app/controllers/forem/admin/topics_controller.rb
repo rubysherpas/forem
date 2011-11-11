eaf10ec26ba8f4e6d4c6315ad10484ffc2f80466
Gabriel Baez <gabrielbaez@bohiti.org>
2011-11-11 11:52:30 -0600

    Lets try fixing the spacing again,  github edit didn't help.

--- app/controllers/forem/admin/topics_controller.rb
+++ app/controllers/forem/admin/topics_controller.rb
@@ -11,7 +11,7 @@ module Forem
         @topic.pinned  = params[:topic][:pinned]
         @topic.locked  = params[:topic][:locked]
         @topic.hidden  = params[:topic][:hidden]
-	@topic.forum_id = params[:topic][:forum_id]
+	      @topic.forum_id = params[:topic][:forum_id]
         if @topic.save
           flash[:notice] = t("forem.topic.updated")
           redirect_to forum_topic_path(@topic.forum, @topic)
