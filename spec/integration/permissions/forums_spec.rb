6218459bc57875d9aedddc1a006db141ab8dc2b1
Ryan Bigg <radarlistener@gmail.com>
2011-11-12 16:11:18 +1100

    stub correct methods in forum permissions spec

--- spec/integration/permissions/forums_spec.rb
+++ spec/integration/permissions/forums_spec.rb
@@ -4,14 +4,15 @@ describe 'forum permissions' do
   let!(:forum) { Factory(:forum) }
 
   it "can't see forums it can't access" do
-    User.any_instance.stub(:can_read_forums?).and_return(false)
+    User.any_instance.stub(:can_read_forem_forums?).and_return(false)
     visit forums_path
+    page!
     page.should_not have_content("Welcome to Forem!")
   end
 
   context "without ability to read all forums" do
     before do
-      User.any_instance.stub(:can_read_forums?).and_return(false)
+      User.any_instance.stub(:can_read_forem_forums?).and_return(false)
     end
 
     it "is denied access" do
@@ -22,7 +23,7 @@ describe 'forum permissions' do
 
   context "without ability to read a specific forum" do
     before do
-      User.any_instance.stub(:can_read_forum?).and_return(false)
+      User.any_instance.stub(:can_read_forem_forum?).and_return(false)
     end
 
     it "is denied access" do
