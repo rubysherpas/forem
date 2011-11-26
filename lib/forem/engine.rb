c9d9bbf4e6b127cb30b3b1b6825ebfce6048d129
Ryan Bigg <radarlistener@gmail.com>
2011-11-26 19:44:54 +1100

    Remove needless after_initialize to load Forem::Ability

--- lib/forem/engine.rb
+++ lib/forem/engine.rb
@@ -8,12 +8,6 @@ module ::Forem
         @root ||= Pathname.new(File.expand_path('../../', __FILE__))
       end
     end
-
-    config.after_initialize do
-      # Add Forem::Ability to whatever Ability class is defined.
-      # Could be Forem's (app/models/ability.rb) or could be application's.
-      ::Ability.send :include, Forem::Ability
-    end
   end
 end
 
