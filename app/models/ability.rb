c64761eba1e78535f7ffbc78daacc04e61ba0f56
Ryan Bigg <radarlistener@gmail.com>
2011-11-26 19:50:55 +1100

    Forem::Ability really is a class now, promise

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,5 +1,6 @@
 # This class should be overriden by your application if you're using CanCan.
 class Ability
+  include CanCan::Ability
   def initialize(user)
   end
 
--- app/models/forem/ability.rb
+++ app/models/forem/ability.rb
@@ -4,25 +4,22 @@ module Forem
   class Ability < ::Ability
     include CanCan::Ability
 
-    def self.included(target)
-      target.class_eval do
+    def initialize(user)
+      # Let other permissions run before this
+      super
 
-        def initialize(user)
-          # Let other permissions run before this
-          super
+      user ||= User.new # anonymous user
+      p user.can_read_forem_forums?
+      p user.can_read_forem_forum?
 
-          user ||= User.new # anonymous user
-
-          if user.can_read_forem_forums?
-            can :read, Forem::Forum do |forum|
-              user.can_read_forem_forum?(forum)
-            end
-          end
-
-          can :read, Forem::Category do |category|
-            user.can_read_forem_category?(category)
-          end
+      if user.can_read_forem_forums?
+        can :read, Forem::Forum do |forum|
+          user.can_read_forem_forum?(forum)
         end
+      end
+
+      can :read, Forem::Category do |category|
+        user.can_read_forem_category?(category)
       end
     end
   end
