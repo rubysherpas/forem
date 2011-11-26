19ceb3f46dfc653a83f9f38ae85c5e769ba475c5
Ryan Bigg <radarlistener@gmail.com>
2011-11-26 19:44:43 +1100

    Forem::Ability is now a class
    This is so we can call super to get to ::Ability's initialize, rather than hack our way to it

--- app/models/forem/ability.rb
+++ app/models/forem/ability.rb
@@ -1,18 +1,15 @@
 require 'cancan'
 
 module Forem
-  module Ability
+  class Ability < ::Ability
     include CanCan::Ability
 
     def self.included(target)
       target.class_eval do
-        # Alias old class's initialize so we can get it to after
-        # Otherwise initialize attempts to go to an initialize that doesn't take an arg
-        alias_method :old_initialize, :initialize
 
         def initialize(user)
           # Let other permissions run before this
-          old_initialize(user)
+          super
 
           user ||= User.new # anonymous user
 
