f76c11bee9d5d640418854767047de835e40e610
Ryan Bigg <radarlistener@gmail.com>
2011-11-26 17:18:54 +1100

    Use user_return_to rather than just 'return_to'
    Devise now requires the scope to be prefixed to the return_to session variable when using it as a location store
    
    Fixes #58

--- app/controllers/forem/application_controller.rb
+++ app/controllers/forem/application_controller.rb
@@ -10,7 +10,7 @@ class Forem::ApplicationController < ApplicationController
 
   def authenticate_forem_user
     if !forem_user
-      session[:return_to] = request.fullpath
+      session["user_return_to"] = request.fullpath
       flash.alert = t("forem.errors.not_signed_in")
       redirect_to main_app.sign_in_path
     end
