be3ff9e574c876eed4446d9ecd1db31ca38a091e
Ryan Bigg <radarlistener@gmail.com>
2011-11-27 18:57:05 +1100

    Add install generator.
    This means that people can install Forem by putting it in their Gemfile, running 'rails g forem:install' and answering a couple of easy questions

---
+++ lib/generators/forem/install_generator.rb
@@ -0,0 +1,123 @@
+require 'rails/generators'
+module Forem
+  module Generators
+    class InstallGenerator < Rails::Generators::Base
+      class_option :user_class, :type => :string
+      class_option "no-migrate", :type => :boolean
+      class_option :current_user_helper, :type => :string
+
+      source_root File.expand_path("../install/templates", __FILE__)
+      desc "Used to install Forem"
+
+      def install_migrations
+        puts "Copying over Forem migrations..."
+        Dir.chdir(Rails.root) do
+          `rake forem:install:migrations`
+        end
+      end
+
+      def add_forem_admin_migration
+        # Is there a cleaner way to do this?
+        if options[:user_class]
+          @user_class = options[:user_class]
+        else
+          @user_class = ask("What is your user class called? [User]")
+        end
+
+        @user_class = User if @user_class.blank?
+        puts "Adding forem_admin migration..."
+
+        last_migration = Dir[Rails.root + "db/migrate/*.rb"].sort.last.split("/").last
+        current_migration_number = /^(\d+)_/.match(last_migration)[1]
+        next_migration_number = ActiveRecord::Migration.next_migration_number(current_migration_number)
+        template "forem_admin_migration.rb", "#{Rails.root}/db/migrate/#{next_migration_number}_add_forem_admin.rb"
+      end
+
+      def add_forem_initializer
+        path = "#{Rails.root}/config/initializers/forem.rb"
+        if File.exists?(path)
+          puts "Skipping config/initializers/forem.rb creation, as file already exists!"
+        else
+          puts "Adding forem initializer (config/initializers/forem.rb)..."
+          template "initializer.rb", path
+        end
+      end
+
+      def run_migrations
+        unless options["no-migrate"]
+          puts "Running rake db:migrate"
+          `rake db:migrate`
+        end
+      end
+
+      def determine_current_user_helper
+        if options[:current_user_helper]
+          current_user_helper = options[:current_user_helper]
+        else
+          current_user_helper = ask("What is the current_user helper called in your app? [current_user]")
+        end
+        current_user_helper = :current_user if current_user_helper.blank?
+        puts "Defining forem_user method inside ApplicationController..."
+
+        inject_into_class("#{Rails.root}/app/controllers/application_controller.rb", ApplicationController) do
+          %Q{
+  def forem_user
+    #{current_user_helper}
+  end
+  helper_method :forem_user}
+        end
+      end
+
+      def mount_engine
+        puts "Mounting Forem::Engine at \"/forums\" in config/routes.rb..."
+        insert_into_file("#{Rails.root}/config/routes.rb", :after => /routes.draw.do\n/) do
+          %Q{  mount Forem::Engine, :at => "/forums"\n}
+        end
+      end
+
+      def finished
+        output = "\n\n" + ("*" * 53)
+        output += %Q{\nDone! Forem has been successfully installed. Yaaaaay!
+
+Here's what happened:\n\n}
+
+        output += step("Forem's migrations were copied over into db/migrate.\n")
+        output += step("We created a new migration called AddForemAdminToTable.
+   This creates a new field called \"forem_admin\" on your #{user_class} model's table.\n")
+        output += step("A new file was created at config/initializers/forem.rb
+   This is where you put Forem's configuration settings.\n")
+        
+        unless options["no-migrate"]
+output += step("`rake db:migrate` was run, running all the migrations against your database.")
+        end
+
+        output += step("A new method called `forem_user` was instered into your ApplicationController.
+   This lets Forem know what the current user of your application is.\n")
+        output += step("The engine was mounted in your config/routes.rb file using this line:
+
+   mount Forem::Engine, :at => \"/forums\"
+
+   If you want to change where the forums are located, just change the \"/forums\" path at the end of this line to whatever you want.")
+        output += %Q{\nAnd finally:
+
+#{step("We told you that Forem has been successfully installed and walked you through the steps.")}}
+        output += "\nIf you have any questions, comments or issues, please post them on our issues page: http://github.com/radar/forem/issues.\n\n"
+        output += "Thanks for using Forem!"
+        puts output
+      end
+
+      private
+
+      def step(words)
+        @step ||= 0
+        @step += 1
+        "#{@step}) #{words}\n"
+      end
+
+      def user_class
+        @user_class.classify.constantize
+      end
+
+    end
+  end
+end
---
+++ spec/generators/install_generator_spec.rb
@@ -0,0 +1,72 @@
+require 'spec_helper'
+require 'generators/forem/install_generator'
+
+describe Forem::Generators::InstallGenerator do
+  before { cleanup! }
+  after { cleanup! }
+
+  # So we can know whether to backup or restore in cleanup!
+  # Wish RSpec had a setting for this already
+  before { flag_example! }
+  def flag_example!
+    example.metadata[:run] = true
+  end
+
+  def backup_file(file)
+    FileUtils.cp(file, file + ".bak")
+  end
+
+  def restore_file(file)
+    FileUtils.mv(file + ".bak", file)
+  end
+
+  def cleanup!
+    Dir.chdir(Rails.root) do
+      FileUtils.rm_rf("db/migrate")
+
+      FileUtils.rm("config/initializers/forem.rb")
+      File.open("config/initializers/forem.rb", "w+") do |f|
+        f.write "Forem.user_class = User"
+      end
+    end
+
+    backup_or_restore = example.metadata[:run] ? "restore" : "backup"
+    backup_or_restore = "#{backup_or_restore}_file"
+    ["#{Rails.root}/app/controllers/application_controller.rb",
+     "#{Rails.root}/config/routes.rb"].each do |file|
+      send(backup_or_restore, file)
+    end
+  end
+
+
+  def migrations
+    Dir["#{Rails.root}/db/migrate/*.rb"].sort
+  end
+
+  it "copies over the migrations" do
+    migrations.should be_empty
+    capture(:stdout) do
+      described_class.start(["--user-class=User", "--no-migrate", "--current_user_helper=current_user"], :destination => Rails.root)
+    end
+
+    # Ensure forem migrations have been cpied over
+    migrations.should_not be_empty
+
+    # Ensure forem admin migration has been created
+    forem_admin_migration = File.readlines(migrations.last)
+    forem_admin_migration[3].strip.should == "add_column :users, :forem_admin, :boolean, :default => false"
+
+    # Ensure initializer has been created
+    forem_initializer = File.readlines("#{Rails.root}/config/initializers/forem.rb")
+    forem_initializer[0].strip.should == "Forem.user_class = User"
+
+    # Ensure forem_user is added to ApplicationController
+    application_controller = File.read("#{Rails.root}/app/controllers/application_controller.rb")
+    expected_forem_user_method = %Q{def forem_user
+    current_user
+  end
+  helper_method :forem_user}
+    application_controller.should include(expected_forem_user_method)
+  end
+
+end
