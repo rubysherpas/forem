37c8c21d2f12ec58d232a5c373ac4c3e4874bae4
Ryan Bigg <radarlistener@gmail.com>
2011-11-27 12:58:38 +1100

    user should not be able to read a topic if they don't have access to that category #53

--- app/models/forem/ability.rb
+++ app/models/forem/ability.rb
@@ -14,7 +14,7 @@ module Forem
 
       if user.can_read_forem_forums?
         can :read, Forem::Forum do |forum|
-          user.can_read_forem_forum?(forum)
+          user.can_read_forem_category?(forum.category) && user.can_read_forem_forum?(forum)
         end
       end
 
