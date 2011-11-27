73687c6c0e979a2cbc5699fc028864a1842d2290
Ryan Bigg <radarlistener@gmail.com>
2011-11-27 11:31:59 +1100

    Move category permissions to be above forum permissions

--- app/models/forem/ability.rb
+++ app/models/forem/ability.rb
@@ -9,14 +9,14 @@ module Forem
 
       user ||= User.new # anonymous user
 
+      can :read, Forem::Category do |category|
+        user.can_read_forem_category?(category)
+      end
+
       if user.can_read_forem_forums?
         can :read, Forem::Forum do |forum|
           user.can_read_forem_forum?(forum)
         end
-      end
-
-      can :read, Forem::Category do |category|
-        user.can_read_forem_category?(category)
       end
     end
   end
