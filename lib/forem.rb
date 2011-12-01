54e13ef4909958b7acaba6743f83ff35e86df773
Ryan Bigg <radarlistener@gmail.com>
2011-12-02 10:37:36 +1100

    user_class is no longer a required setting
    This is because rails g forem:install (i.e. the step you're supposed to do RIGHT AFTER adding forem to the Gemfile) takes care of setting this up. If it's not set, you've done something wrong.

--- lib/forem.rb
+++ lib/forem.rb
@@ -6,19 +6,4 @@ module Forem
   mattr_accessor :user_class, :theme, :formatter, :default_gravatar, :default_gravatar_image,
                  :user_profile_links
 
-  class << self
-    def user_class
-      @@user_class || raise(ConfigurationNotFound.new("user_class"))
-    end
-  end
-
-  class ConfigurationNotFound < StandardError
-    attr_accessor :message
-    def initialize(option)
-      @message = "Forem configuration option #{option} not found. " +
-        "Please set this in config/initializers/forem.rb with this line:\n\n" +
-        "Forem.#{option}= <value>"
-    end
-  end
-
 end
