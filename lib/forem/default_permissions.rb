af61b8763db3c31c558100dc755cd0cfe704e338
Ryan Bigg <radarlistener@gmail.com>
2011-12-07 09:29:56 +1100

    Should be using method_defined?, not respond_to?
    method_defined? will check for instance methods where as respond_to? will only check on the current object (which in this case is a User-like class)
    
    Thanks to @browning for pointing this one out

--- lib/forem/default_permissions.rb
+++ lib/forem/default_permissions.rb
@@ -5,31 +5,31 @@ module Forem
     extend ActiveSupport::Concern
 
     included do
-      unless respond_to?(:can_read_forem_category?)
+      unless method_defined?(:can_read_forem_category?)
         def can_read_forem_category?(category)
           true
         end
       end
 
-      unless respond_to?(:can_read_forem_forums?)
+      unless method_defined?(:can_read_forem_forums?)
         def can_read_forem_forums?
           true
         end
       end
 
-      unless respond_to?(:can_read_forem_forum?)
+      unless method_defined?(:can_read_forem_forum?)
         def can_read_forem_forum?(forum)
           true
         end
       end
 
-      unless respond_to?(:can_create_forem_topics?)
+      unless method_defined?(:can_create_forem_topics?)
         def can_create_forem_topics?(forum)
           true
         end
       end
 
-      unless respond_to?(:can_reply_to_forem_topic?)
+      unless method_defined?(:can_reply_to_forem_topic?)
         def can_reply_to_forem_topic?(topic)
           true
         end
