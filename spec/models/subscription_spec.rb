d464322d3306917eecbbf37c0e9432b5dd181eba
AstonJ <email@astonj.com>
2011-12-08 19:00:09 +0000

    Added subcription_spec, subscription model and associations

---
+++ spec/models/subscription_spec.rb
@@ -0,0 +1,21 @@
+require 'spec_helper'
+
+describe Forem::Subscription do
+  describe "topic subscriptions" do
+    before(:each) do
+      Forem::Topic.any_instance.stub(:set_first_post_user)
+      attr = {
+        :subject => "A topic"
+      }
+      @topic = Forem::Topic.create!(attr)
+    end
+    
+    it "creates a subscription when a topic is created" do
+      @topic.subscriptions.count.should == 1
+    end
+    
+    it "adds the topic subscriber to the subscription" do
+      @topic.subscriptions.first.subscriber.should == @topic.user
+    end
+  end
+end
