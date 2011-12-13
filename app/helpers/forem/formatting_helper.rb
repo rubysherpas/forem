92bcc496ca11147097d18daf05a1ebf22891cfe1
John Gadbois <jgadbois@gmail.com>
2011-12-13 09:35:10 -0500

    Add as_sanitized_text method

--- app/helpers/forem/formatting_helper.rb
+++ app/helpers/forem/formatting_helper.rb
@@ -3,7 +3,7 @@ module Forem
     # override with desired markup formatter, e.g. textile or markdown
     def as_formatted_html(text)
       if Forem.formatter
-        Forem.formatter.format(sanitize(text, :tags=>%W(p), :attributes=>[]))
+        Forem.formatter.format(as_sanitized_text(text))
       else
         simple_format(h(text))
       end
@@ -11,10 +11,14 @@ module Forem
 
     def as_quoted_text(text)
       if Forem.formatter && Forem.formatter.respond_to?(:blockquote)
-        Forem.formatter.blockquote(h(text)).html_safe
+        Forem.formatter.blockquote(as_sanitized_text(text)).html_safe
       else
          "<blockquote>#{(h(text))}</blockquote>\n\n".html_safe
       end
+    end
+
+    def as_sanitized_text(text)
+      sanitize(text, :tags=>%W(p), :attributes=>[])
     end
   end
 end
