9249b844c29894fb16c112ddf02aad9baec1c9c4
John Gadbois <jgadbois@gmail.com>
2011-12-28 16:58:07 -0500

    Add email contents

---
+++ spec/mailers/subscription_mailer_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+
+describe Forem::SubscriptionMailer do
+  describe "topic_reply" do
+    let(:user) { Factory(:user) }
+    let(:topic) { Factory(:topic) }
+    let(:post) { Factory(:post, :topic=>topic) }
+    let(:mail) { Forem::SubscriptionMailer.topic_reply(post.id, user.id) }
+
+		it "sends an email announcing a forum post update" do
+			mail.to.should eq([user.email])
+			mail.subject.should eq("A topic you are subscribed to has received a reply")
+      mail.body.encoded.should match(forum_topic_url(topic.forum, topic))
+		end
+	end
+end
