471d1585bd37abaae557819b948a33b1599aad48
Ryan Bigg <radarlistener@gmail.com>
2012-01-09 09:11:54 +1100

    Wrap topics_controller test in a context, as the before block is only really good for the one tesT

--- spec/controllers/topics_controller_spec.rb
+++ spec/controllers/topics_controller_spec.rb
@@ -1,21 +1,21 @@
 require 'spec_helper'
 
 describe Forem::TopicsController do
+  context "attempting to subscribe to a hidden topic" do
+    before do
+      sign_in(:user, Factory(:user))
 
-  before do
-    sign_in(:user, Factory(:user))
+      forum = stub_model(Forem::Forum)
 
-    forum = stub_model(Forem::Forum)
+      controller.should_receive(:authorize!).and_return(true)
+      Forem::Forum.should_receive(:find).and_return(forum)
+      forum.stub_chain("topics.visible.find").and_raise(ActiveRecord::RecordNotFound)
+    end
 
-    controller.should_receive(:authorize!).and_return(true)
-    Forem::Forum.should_receive(:find).and_return(forum)
-    forum.stub_chain("topics.visible.find").and_raise(ActiveRecord::RecordNotFound)
+    # Regression test for #122
+    specify do
+      get :subscribe, :forum_id => 1, :id => 1
+      flash[:alert].should == "The topic you are looking for could not be found."
+    end
   end
-
-  # Regression test for #122
-  it "cannot subscribe to a hidden topic" do
-    get :subscribe, :forum_id => 1, :id => 1
-    flash[:alert].should == "The topic you are looking for could not be found."
-  end
-
 end
