8f8c56bc7f2ec58fb0d53ee278825cbc618b14dc
Ryan Bigg <radarlistener@gmail.com>
2012-01-09 09:10:48 +1100

    Use stubbing in topics_controller_spec FOR SPEED

--- spec/controllers/topics_controller_spec.rb
+++ spec/controllers/topics_controller_spec.rb
@@ -1,16 +1,20 @@
 require 'spec_helper'
 
 describe Forem::TopicsController do
-  let(:visible_topic) { Factory(:topic) }
-  let(:hidden_topic) { Factory(:topic, :hidden => true) }
 
   before do
     sign_in(:user, Factory(:user))
+
+    forum = stub_model(Forem::Forum)
+
+    controller.should_receive(:authorize!).and_return(true)
+    Forem::Forum.should_receive(:find).and_return(forum)
+    forum.stub_chain("topics.visible.find").and_raise(ActiveRecord::RecordNotFound)
   end
 
   # Regression test for #122
   it "cannot subscribe to a hidden topic" do
-    get :subscribe, :forum_id => hidden_topic.forum_id, :id => hidden_topic.id
+    get :subscribe, :forum_id => 1, :id => 1
     flash[:alert].should == "The topic you are looking for could not be found."
   end
 
