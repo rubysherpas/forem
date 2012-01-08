c4c1942d684b34ddba012f16a1d7c60410ca3d94
Ryan Bigg <radarlistener@gmail.com>
2012-01-09 08:26:52 +1100

    Remove tabs from post_spec

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -13,33 +13,33 @@ describe Forem::Post do
     end
   end
 
-	context "after creation" do
-		it "subscribes the current poster" do
-			@topic = FactoryGirl.create(:topic)
-			@post = FactoryGirl.create(:post, :topic => @topic)
-			@topic.subscriptions.last.subscriber.should == @post.user
-		end
+  context "after creation" do
+    it "subscribes the current poster" do
+      @topic = FactoryGirl.create(:topic)
+      @post = FactoryGirl.create(:post, :topic => @topic)
+      @topic.subscriptions.last.subscriber.should == @post.user
+    end
 
-		it "emails subscribers after post creation" do
-			@post = FactoryGirl.build(:post)
-			@post.should_receive(:email_topic_subscribers)
-			@post.save
-		end
+    it "emails subscribers after post creation" do
+      @post = FactoryGirl.build(:post)
+      @post.should_receive(:email_topic_subscribers)
+      @post.save
+    end
 
-		it "only emails other subscribers" do
-			@user1 = FactoryGirl.create(:user)
-			@user2 = FactoryGirl.create(:user)
-			@topic = FactoryGirl.create(:topic)
-			@post = FactoryGirl.create(:post, :topic => @topic, :user => @user2)
-			subs = [ Forem::Subscription.create(:topic => @topic, :subscriber => @user1), Forem::Subscription.create(:topic => @topic, :subscriber => @user2)]
-			@post.topic.stub(:subscriptions).and_return(subs);
-			@post.topic.stub_chain([:subscriptions, :includes]).and_return(subs)
+    it "only emails other subscribers" do
+      @user1 = FactoryGirl.create(:user)
+      @user2 = FactoryGirl.create(:user)
+      @topic = FactoryGirl.create(:topic)
+      @post = FactoryGirl.create(:post, :topic => @topic, :user => @user2)
+      subs = [ Forem::Subscription.create(:topic => @topic, :subscriber => @user1), Forem::Subscription.create(:topic => @topic, :subscriber => @user2)]
+      @post.topic.stub(:subscriptions).and_return(subs);
+      @post.topic.stub_chain([:subscriptions, :includes]).and_return(subs)
 
-			@post.topic.subscriptions.first.should_receive(:send_notification)
-			@post.topic.subscriptions.last.should_not_receive(:send_notification)
-			@post.email_topic_subscribers
-		end
-	end
+      @post.topic.subscriptions.first.should_receive(:send_notification)
+      @post.topic.subscriptions.last.should_not_receive(:send_notification)
+      @post.email_topic_subscribers
+    end
+  end
 
   context "helper methods" do
     it "retrieves the topic's forum" do
