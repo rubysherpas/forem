1a6d352b4183c5c6bcda83a5a31bd176423d56c7
Ryan Bigg <radarlistener@gmail.com>
2012-01-09 08:24:21 +1100

    no more tabs kthxbyeee

--- spec/mailers/subscription_mailer_spec.rb
+++ spec/mailers/subscription_mailer_spec.rb
@@ -7,14 +7,14 @@ describe Forem::SubscriptionMailer do
     let(:post) { Factory(:post, :topic=>topic) }
     let(:mail) { Forem::SubscriptionMailer.topic_reply(post.id, user.id) }
 
-		it "sends an email announcing a forum post update" do
-			mail.to.should eq([user.email])
-			mail.subject.should eq("A topic you are subscribed to has received a reply")
-      mail.body.encoded.should match(forum_topic_url(topic.forum, topic))
-		end
+  it "sends an email announcing a forum post update" do
+    mail.to.should eq([user.email])
+    mail.subject.should eq("A topic you are subscribed to has received a reply")
+    mail.body.encoded.should match(forum_topic_url(topic.forum, topic))
+  end
 
     it "contains an unsubscribe link" do
       mail.body.encoded.should match(unsubscribe_topic_path(topic))
     end
-	end
+  end
 end
