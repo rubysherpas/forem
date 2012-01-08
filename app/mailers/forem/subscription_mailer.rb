c43880ea4cef237196677e31fb07359b8173ac82
Ryan Bigg <radarlistener@gmail.com>
2012-01-09 08:40:11 +1100

    Fix tabs in SubscriptionMailer

--- app/mailers/forem/subscription_mailer.rb
+++ app/mailers/forem/subscription_mailer.rb
@@ -1,14 +1,13 @@
 module Forem
   class SubscriptionMailer < ActionMailer::Base
-		# TODO: Make this configurable upon installation
     default :from => Forem.email_from_address
 
     def topic_reply(post_id, subscriber_id)
-			# only pass id to make it easier to send emails using resque
-			@post = Post.find(post_id)
-			@user = User.find(subscriber_id)
+      # only pass id to make it easier to send emails using resque
+      @post = Post.find(post_id)
+      @user = User.find(subscriber_id)
 
-			mail(:to => @user.email, :subject => "A topic you are subscribed to has received a reply")
-		end
+      mail(:to => @user.email, :subject => "A topic you are subscribed to has received a reply")
+    end
   end
 end
