f3936c708b03ab493fc0fe9a11dc4b53cddb2b41
Ryan Bigg <radarlistener@gmail.com>
2012-02-08 08:02:12 +1100

    Add comment to initializer that shows how you can customize the layout

--- lib/generators/forem/install/templates/initializer.rb
+++ lib/generators/forem/install/templates/initializer.rb
@@ -1,2 +1,10 @@
 Forem.user_class = "<%= user_class %>"
 Forem.email_from_address = "please-change-me@example.com"
+
+# If you want to change the layout that Forem uses, uncomment and customize these lines:
+#
+# Rails.application.config.to_prepare do
+#   Forem::ApplicationController.layout "forem"
+# end
+#
+# By default, these lines will use the layout located at app/views/layouts/forem.html.erb in your application.
