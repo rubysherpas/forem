337e22a8f64517ffc946f5b67d68066c836e57b8
Ryan Bigg <radarlistener@gmail.com>
2012-02-24 22:19:08 -0500

    Stub forem_group_ids, not group_ids, in Forum#moderator? test

--- spec/models/forum_spec.rb
+++ spec/models/forum_spec.rb
@@ -58,7 +58,7 @@ describe Forem::Forum do
 
       it "is a moderator if group ids intersect" do
         @forum.stub :moderator_ids => [1,2]
-        user = stub :group_ids => [2,3]
+        user = stub :forem_group_ids => [2,3]
         @forum.moderator?(user).should be_true
       end
 
