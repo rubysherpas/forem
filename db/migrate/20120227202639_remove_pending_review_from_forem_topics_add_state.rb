620a071a159e0d11d114dccb8aeba59106fe2362
Ryan Bigg <radarlistener@gmail.com>
2012-02-27 17:43:25 -0500

    Add missing state migrations for posts and topics

---
+++ db/migrate/20120227202639_remove_pending_review_from_forem_topics_add_state.rb
@@ -0,0 +1,10 @@
+class RemovePendingReviewFromForemTopicsAddState < ActiveRecord::Migration
+  def change
+    remove_column :forem_topics, :pending_review
+    add_column :forem_topics, :state, :string, :default => 'pending_review'
+    add_index :forem_topics, :state
+  end
+
+  def down
+  end
+end
