a8e86e0b6dddd165eec66dfb25a89bdd00282483
Ryan Bigg <radarlistener@gmail.com>
2012-02-27 11:52:19 -0500

    Stub out all calls to user in topic model spec
    Because we're testing Topic, not User

--- spec/models/subscription_spec.rb
+++ spec/models/subscription_spec.rb
@@ -4,17 +4,17 @@ describe Forem::Subscription do
   describe "topic subscriptions" do
     before(:each) do
       Forem::Topic.any_instance.stub(:set_first_post_user)
+      Forem::Topic.any_instance.stub(:user).and_return(User)
       attr = {
         :subject => "A topic",
-        :user_id => Factory.create(:user).id
       }
       @topic = Forem::Topic.create!(attr)
     end
-    
+
     it "creates a subscription when a topic is created" do
       @topic.subscriptions.count.should == 1
     end
-    
+
     it "adds the topic subscriber to the subscription" do
       @topic.subscriptions.first.subscriber.should == @topic.user
     end
