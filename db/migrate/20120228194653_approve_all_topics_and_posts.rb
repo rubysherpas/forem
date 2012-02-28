3f5fd0c61b8c796bc6b24ee34dd32ed9f294284a
Ryan Bigg <radarlistener@gmail.com>
2012-02-28 16:49:07 -0500

    Call update_attribute to skip all callbacks on topic and post inside approve all topics and posts migration

--- db/migrate/20120228194653_approve_all_topics_and_posts.rb
+++ db/migrate/20120228194653_approve_all_topics_and_posts.rb
@@ -1,15 +1,7 @@
 class ApproveAllTopicsAndPosts < ActiveRecord::Migration
-  # Stub out Forem::Post.notified?
-  Forem::Post
-  class Forem::Post < ActiveRecord::Base
-    def notified?
-      true
-    end
-  end
-
   def up
-    Forem::Topic.find_each { |t| t.approve! }
-    Forem::Post.find_each { |t| t.approve! }
+    Forem::Topic.find_each { |t| t.update_attribute(:state => "approved") }
+    Forem::Post.find_each { |p| p.update_attribute(:state => "approved") }
   end
 
   def down
