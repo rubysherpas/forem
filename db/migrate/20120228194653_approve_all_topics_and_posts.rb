5047e22f0394ff4887e546f5830f26a6f65c495f
Ryan Bigg <radarlistener@gmail.com>
2012-02-28 16:46:27 -0500

    Stub out Forem::Post.notified? in a migration that may run before it is defined

--- db/migrate/20120228194653_approve_all_topics_and_posts.rb
+++ db/migrate/20120228194653_approve_all_topics_and_posts.rb
@@ -1,4 +1,12 @@
 class ApproveAllTopicsAndPosts < ActiveRecord::Migration
+  # Stub out Forem::Post.notified?
+  Forem::Post
+  class Forem::Post < ActiveRecord::Base
+    def notified?
+      true
+    end
+  end
+
   def up
     Forem::Topic.find_each { |t| t.approve! }
     Forem::Post.find_each { |t| t.approve! }
