b2869a11e08c691eec9fece8c5deb79ab012f4fc
Ryan Bigg <radarlistener@gmail.com>
2012-03-02 06:33:29 -0800

    Revert "Pass full objects to subscription_mailer to save having to find them again"
    As this would break Resque::Mailer as pointed out by glebm here:
    https://github.com/radar/forem/commit/23aa3dadd969ba6417f1e4b3a56d4eb44f16c2fb#commitcomment-1034615
    
    This reverts commit 23aa3dadd969ba6417f1e4b3a56d4eb44f16c2fb.

--- app/mailers/forem/subscription_mailer.rb
+++ app/mailers/forem/subscription_mailer.rb
@@ -2,11 +2,12 @@ module Forem
   class SubscriptionMailer < ActionMailer::Base
     default :from => Forem.email_from_address
 
-    def topic_reply(post, subscriber)
-      @post = post
-      @subscriber = subscriber
+    def topic_reply(post_id, subscriber_id)
+      # only pass id to make it easier to send emails using resque
+      @post = Post.find(post_id)
+      @user = Forem.user_class.find(subscriber_id)
 
-      mail(:to => @subscriber.email, :subject => "A topic you are subscribed to has received a reply")
+      mail(:to => @user.email, :subject => "A topic you are subscribed to has received a reply")
     end
   end
 end
