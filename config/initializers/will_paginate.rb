83e86b6b808e8dafac5eaf47ef2acf31fe45bffc
Reginald Tan <redge.tan@gmail.com>
2012-03-27 13:40:56 -0400

    Extend will_paginate only when ActiveRecord::Base is loaded
    Fixes #183

--- config/initializers/will_paginate.rb
+++ config/initializers/will_paginate.rb
@@ -1,9 +1,11 @@
 if defined?(WillPaginate)
-  module WillPaginate
-    module ActiveRecord
-      module RelationMethods
-        alias_method :per, :per_page
-        alias_method :num_pages, :total_pages
+  ActiveSupport.on_load :active_record do
+    module WillPaginate
+      module ActiveRecord
+        module RelationMethods
+          alias_method :per, :per_page
+          alias_method :num_pages, :total_pages
+        end
       end
     end
   end
