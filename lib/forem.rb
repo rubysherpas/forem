049057a8e94e59610365b6764c733eb7876c8351
Ryan Bigg <radarlistener@gmail.com>
2012-03-29 07:18:02 -0500

    Include kernel/singleton_class extension so that we can run tests on 1.8.7

--- lib/forem.rb
+++ lib/forem.rb
@@ -1,3 +1,6 @@
+# Fix for #185 and build issues
+require 'active_support/core_ext/kernel/singleton_class'
+
 require 'forem/engine'
 require 'forem/autocomplete'
 require 'forem/default_permissions'
