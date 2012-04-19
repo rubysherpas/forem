7995ee6cfb13943b24beff2a163e6ef41060f961
tanraya <demerest@gmail.com>
2012-04-20 01:52:52 +0600

    Code cleanups and aligning for models

--- app/models/forem/view.rb
+++ app/models/forem/view.rb
@@ -5,7 +5,7 @@ module Forem
     belongs_to :viewable, :polymorphic => true
     belongs_to :user, :class_name => Forem.user_class.to_s
 
-    validates :viewable_id, :presence => true
+    validates :viewable_id,   :presence => true
     validates :viewable_type, :presence => true
 
     def viewed_at
