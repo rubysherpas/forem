5600cf5059c44f3f90bf0e90cab7e661c605d183
John Gadbois <jgadbois@gmail.com>
2012-04-25 15:25:18 -0400

    Forem.base_path does not have trailing slash

--- app/assets/javascripts/forem/admin/members.js
+++ app/assets/javascripts/forem/admin/members.js
@@ -5,7 +5,7 @@
 //
 $(document).ready(function() {
   group_id = $('#add_member').data('group-id');
-  $('#new_member').autocomplete({source: Forem.base_path + 'admin/users/autocomplete'})
+  $('#new_member').autocomplete({source: Forem.base_path + '/admin/users/autocomplete'})
 
   $("#new_member").bind("autocompleteselect", function(event, ui) {
     $("#add_member").attr('disabled', false)
@@ -13,7 +13,7 @@ $(document).ready(function() {
 
   add_member = function() {
     user = $("#new_member").val()
-    $.post(Forem.base_path + 'admin/groups/' + group_id + '/members', { user: user })
+    $.post(Forem.base_path + '/admin/groups/' + group_id + '/members', { user: user })
     $(this).attr('disabled', true)
     $("#new_member").val("")
     $('#members').append('<li>' + user + '</li>')
