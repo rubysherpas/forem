b9a9731385e68bafa6b71688e04c0d089b2d407d
Juan Felipe Alvarez Saldarriaga <nebiros@gmail.com>
2012-06-14 15:25:35 -0500

    fixed issue: https://github.com/radar/forem/issues/183#issuecomment-6334720, see: https://github.com/mislav/will_paginate/issues/174#issuecomment-4475139.

--- config/initializers/will_paginate.rb
+++ config/initializers/will_paginate.rb
@@ -3,9 +3,12 @@ if defined?(WillPaginate)
     module WillPaginate
       module ActiveRecord
         module RelationMethods
-          alias_method :per, :per_page
-          alias_method :num_pages, :total_pages
+          def per(value = nil) per_page(value) end
+          def total_count() count end
         end
+      end
+      module CollectionMethods
+        alias_method :num_pages, :total_pages
       end
     end
   end
