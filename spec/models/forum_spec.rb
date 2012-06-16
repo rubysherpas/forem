b9f9b82b308fe6e9425c0cb18d9391a2f367016b
Ryan Bigg <radarlistener@gmail.com>
2012-06-16 10:26:58 +1000

    Use let rather than instance variable in forum_spec

--- spec/models/forum_spec.rb
+++ spec/models/forum_spec.rb
@@ -1,42 +1,34 @@
 require 'spec_helper'
 
 describe Forem::Forum do
-  before(:each) do
-    category = FactoryGirl.create(:category)
-    @attr = {
-      :title => "A forum",
-      :description => "My sweet forum of goodness",
-      :category_id => category.id
-    }
-    @forum = Forem::Forum.create!(@attr)
-  end
-  
+  let!(:forum) { FactoryGirl.create(:forum) }
+
   it "is valid with valid attributes" do
-    @forum.should be_valid
+    forum.should be_valid
   end
   
   describe "validations" do
     it "requires a title" do
-      @forum.title = nil
-      @forum.should_not be_valid
+      forum.title = nil
+      forum.should_not be_valid
     end
 
     it "requires a description" do
-      @forum.description = nil
-      @forum.should_not be_valid
+      forum.description = nil
+      forum.should_not be_valid
     end
 
     it "requires a category id" do
-      @forum.category_id = nil
-      @forum.should_not be_valid
+      forum.category_id = nil
+      forum.should_not be_valid
     end
   end
 
   describe "helper methods" do
     # Regression tests + tests related to fix for #42
     context "last_post" do
-      let!(:visible_topic) { FactoryGirl.create(:topic, :forum => @forum) }
-      let!(:hidden_topic) { FactoryGirl.create(:topic, :forum => @forum, :hidden => true) }
+      let!(:visible_topic) { FactoryGirl.create(:topic, :forum => forum) }
+      let!(:hidden_topic) { FactoryGirl.create(:topic, :forum => forum, :hidden => true) }
 
       let(:user) { FactoryGirl.create(:user) }
       let(:admin) { FactoryGirl.create(:admin) }
@@ -44,7 +36,7 @@ describe Forem::Forum do
 
       context "finding the last visible post for a user" do
         it "does not find non-approved posts" do
-          @forum.last_visible_post(user).should be_nil
+          forum.last_visible_post(user).should be_nil
         end
 
         context "with approved topic + post" do
@@ -55,16 +47,16 @@ describe Forem::Forum do
           end
 
           it "finds the last post for a user" do
-            @forum.last_visible_post(user).should == visible_topic.posts.last
+            forum.last_visible_post(user).should == visible_topic.posts.last
             # visible post doesn't contain hidden topics, duh
-            @forum.last_visible_post(admin).should == visible_topic.posts.last
+            forum.last_visible_post(admin).should == visible_topic.posts.last
           end
         end
       end
 
       context "finding the last post for a user" do
         it "does not find non-approved posts" do
-          @forum.last_post_for(user).should be_nil
+          forum.last_post_for(user).should be_nil
         end
 
         context "with approved topic + post" do
@@ -75,8 +67,8 @@ describe Forem::Forum do
           end
 
           it "finds the last post for a user" do
-            @forum.last_post_for(user).should == visible_topic.posts.last
-            @forum.last_post_for(admin).should == hidden_topic.posts.last
+            forum.last_post_for(user).should == visible_topic.posts.last
+            forum.last_post_for(admin).should == hidden_topic.posts.last
           end
         end
       end
@@ -84,13 +76,13 @@ describe Forem::Forum do
 
     context "moderator?" do
       it "no user is no moderator" do
-        @forum.moderator?(nil).should be_false
+        forum.moderator?(nil).should be_false
       end
 
       it "is a moderator if group ids intersect" do
-        @forum.stub :moderator_ids => [1,2]
+        forum.stub :moderator_ids => [1,2]
         user = stub :forem_group_ids => [2,3]
-        @forum.moderator?(user).should be_true
+        forum.moderator?(user).should be_true
       end
 
     end
