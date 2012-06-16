2fa055c3e9914867c5dddb953b334bb626e52365
Ryan Bigg <radarlistener@gmail.com>
2012-06-16 10:27:16 +1000

    Use warden helpers rather than capybara walking through signing in for sign_in

--- spec/support/sign_in_helpers.rb
+++ spec/support/sign_in_helpers.rb
@@ -1,18 +1,19 @@
 module SignInHelpers
   def sign_in(user)
-    visit '/users/sign_in'
-    fill_in 'Email', :with => user.email
-    fill_in 'Password', :with => 'password'
-    click_button 'Sign in'
-    page.should have_content("Signed in successfully")
+    login_as(user)
+    # visit '/users/sign_in'
+    # fill_in 'Email', :with => user.email
+    # fill_in 'Password', :with => 'password'
+    # click_button 'Sign in'
+    # page.should have_content("Signed in successfully")
   end
 
   def sign_out
-    click_link 'Sign out'
-    page.should have_content("Signed out successfully")
+    logout
   end
 end
 
 RSpec.configure do |c|
+  c.include Warden::Test::Helpers
   c.include SignInHelpers, :type => :request
 end
