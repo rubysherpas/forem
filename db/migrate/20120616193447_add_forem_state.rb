f44662c320f012a0756aeb8d813a1a792ac555ed
Philip Arndt <parndt@gmail.com>
2012-06-29 11:21:21 +1200

    Removed Rails auto-generated migration comments

--- db/migrate/20120616193447_add_forem_state.rb
+++ db/migrate/20120616193447_add_forem_state.rb
@@ -1,11 +1,10 @@
-# This migration comes from forem (originally 20120616000459)
 class AddForemState < ActiveRecord::Migration
   def change
     unless column_exists?(user_class, :forem_state)
       add_column user_class, :forem_state, :string, :default => 'pending_review'
     end
   end
-  
+
   def user_class
     Forem.user_class.table_name.downcase.to_sym
   end
--- db/migrate/20120616193448_add_forem_auto_subscribe.rb
+++ db/migrate/20120616193448_add_forem_auto_subscribe.rb
@@ -1,4 +1,3 @@
-# This migration comes from forem (originally 20120616000935)
 class AddForemAutoSubscribe < ActiveRecord::Migration
   def change
     unless column_exists?(user_class, :forem_admin)
