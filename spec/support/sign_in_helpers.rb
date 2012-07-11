c5c75c13ae7a2e34088e5bb0e7cbe2ba82808f3b
Ryan Bigg <radarlistener@gmail.com>
2012-07-11 18:09:03 +1000

    Remove comments from sign_in_helpers

--- spec/support/sign_in_helpers.rb
+++ spec/support/sign_in_helpers.rb
@@ -1,11 +1,6 @@
 module SignInHelpers
   def sign_in(user)
     login_as(user)
-    # visit '/users/sign_in'
-    # fill_in 'Email', :with => user.email
-    # fill_in 'Password', :with => 'password'
-    # click_button 'Sign in'
-    # page.should have_content("Signed in successfully")
   end
 
   def sign_out
