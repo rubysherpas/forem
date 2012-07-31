8d980dd5cebb7bce4b281eedad79c73ddfc55fcf
ch000 <github_@christian-warnecke.de>
2012-07-31 16:36:09 +0300

    Update app/models/forem/ability.rb
    issue:
    https://github.com/radar/forem/issues/265
    source from spree:
    https://github.com/spree/spree/blob/master/core/app/models/spree/ability.rb#L17-19
    
    
    added self.register_ability(ability) and self.remove_ability(ability)
    
    # Allows us to go beyond the standard cancan initialize method which makes it difficult for engines to
    # modify the default +Ability+ of an application.  The +ability+ argument must be a class that includes
    # the +CanCan::Ability+ module.  The registered ability should behave properly as a stand-alone class
    # and therefore should be easy to test in isolation.

--- app/models/forem/ability.rb
+++ app/models/forem/ability.rb
@@ -3,6 +3,21 @@ require 'cancan'
 module Forem
   class Ability
     include CanCan::Ability
+    
+    class_attribute :abilities
+    self.abilities = Set.new
+    
+    # Allows us to go beyond the standard cancan initialize method which makes it difficult for engines to
+    # modify the default +Ability+ of an application.  The +ability+ argument must be a class that includes
+    # the +CanCan::Ability+ module.  The registered ability should behave properly as a stand-alone class
+    # and therefore should be easy to test in isolation.
+    def self.register_ability(ability)
+      self.abilities.add(ability)
+    end
+
+    def self.remove_ability(ability)
+      self.abilities.delete(ability)
+    end
 
     def initialize(user)
       user ||= Forem.user_class.new
