164d96b8c24c8beea408050b431f10c0245191e1
Josh Adams <josh@isotope11.com>
2012-10-02 21:27:43 -0500

    Get gemoji in, and tests still pass... :cat:

--- lib/forem/engine.rb
+++ lib/forem/engine.rb
@@ -33,6 +33,7 @@ module ::Forem
 end
 
 require 'simple_form'
+require 'emoji/railtie'
 
 # We need one of the two pagination engines loaded by this point.
 # We don't care which one, just one of them will do.
