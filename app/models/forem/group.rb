0007edb62692e30abae7ee0ed492876234a01a77
Mike Chai <mike@weird.name>
2012-10-12 14:36:32 -0700

    bad commit, testing installation

--- app/models/forem/group.rb
+++ app/models/forem/group.rb
@@ -3,9 +3,13 @@ module Forem
     validates :name, :presence => true
 
     has_many :memberships
-    t_has_many :members, :through => :memberships, :class_name => Forem.user_class.to_s
+    #t_has_many :members, :through => :memberships, :class_name => Forem.user_class.to_s
 
     attr_accessible :name
+    
+    def members
+      []
+    end
 
     def to_s
       name
