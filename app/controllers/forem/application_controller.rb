cac3965d2b4b9be42d0ef5d5caa2790047c4d62d
Ryan Barry <ryanc.barry@yahoo.com>
2012-10-22 16:39:18 -0700

    Updated forem views

--- app/controllers/forem/application_controller.rb
+++ app/controllers/forem/application_controller.rb
@@ -1,6 +1,8 @@
 require 'cancan'
 
 class Forem::ApplicationController < ApplicationController
+  layout 'fullwidth'
+
   rescue_from CanCan::AccessDenied do
     redirect_to root_path, :alert => t("forem.access_denied")
   end
