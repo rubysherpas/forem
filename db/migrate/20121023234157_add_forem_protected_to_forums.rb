bc7cd8eda4f48d1d8a0072a8f2a1f25a803f845f
Mike Chai <mike@weird.name>
2012-10-30 11:40:48 -0700

    added check for existing column in forem_protected migration

--- db/migrate/20121023234157_add_forem_protected_to_forums.rb
+++ db/migrate/20121023234157_add_forem_protected_to_forums.rb
@@ -1,5 +1,5 @@
 class AddForemProtectedToForums < ActiveRecord::Migration
   def change
-    add_column :forem_forums, :forem_protected, :boolean, :default => false
+    add_column :forem_forums, :forem_protected, :boolean, :default => false unless column_exists?(:forem_forums, :forem_protected)
   end
 end
