6695a97a9945df2ed9d2ec3cb65096af2afd8c42
mikool <mikool@mikools-MacBook-Air.local>
2012-11-06 16:56:31 -0800

    fixed admin layout not showing up

--- app/controllers/forem/admin/base_controller.rb
+++ app/controllers/forem/admin/base_controller.rb
@@ -2,6 +2,7 @@ module Forem
   module Admin
     class BaseController < Forem::ApplicationLogController
       before_filter :authenticate_forem_admin
+      layout 'forem/application'
 
       def index
         # TODO: perhaps gather some stats here to show on the admin page?
