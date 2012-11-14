a974d46f08c662cac49f0ca7981efffe40b6bb7c
Ryan Bigg <radarlistener@gmail.com>
2012-11-15 09:53:46 +1100

    Fix emojis path
    Fixes #331

--- app/helpers/forem/application_helper.rb
+++ app/helpers/forem/application_helper.rb
@@ -43,7 +43,7 @@ module Forem
     def forem_emojify(content)
       h(content).to_str.gsub(/:([a-z0-9\+\-_]+):/) do |match|
         if Emoji.names.include?($1)
-          '<img alt="' + $1 + '" height="20" src="' + asset_path("emoji/#{$1}.png") + '" style="vertical-align:middle" width="20" />'
+          '<img alt="' + $1 + '" height="20" src="' + asset_path("emojis/#{$1}.png") + '" style="vertical-align:middle" width="20" />'
         else
           match
         end
