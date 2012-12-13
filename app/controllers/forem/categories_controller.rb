88713e2f128a2d7b1f3cc13ae739b282c05d6bd4
Marcello Barnaba <vjt@openssl.it>
2012-12-13 16:36:36 +0100

    Use class names, not constants

--- app/controllers/forem/categories_controller.rb
+++ app/controllers/forem/categories_controller.rb
@@ -1,6 +1,6 @@
 module Forem
   class CategoriesController < Forem::ApplicationController
     helper 'forem/forums'
-    load_and_authorize_resource :class => Forem::Category
+    load_and_authorize_resource :class => 'Forem::Category'
   end
 end
