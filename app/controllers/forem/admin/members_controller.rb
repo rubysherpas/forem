03725d78827c235574b0186350316821c880a7e9
Ryan Bigg <radarlistener@gmail.com>
2012-12-28 14:57:21 +1030

    Fix MembersController inherited class to be Forem::Admin::BaseController

--- app/controllers/forem/admin/members_controller.rb
+++ app/controllers/forem/admin/members_controller.rb
@@ -1,18 +1,19 @@
 module Forem
-  class Admin::MembersController < ApplicationController
-
-    def create
-      user = Forem.user_class.where(Forem.autocomplete_field => params[:user]).first
-      unless group.members.exists?(user.id)
-        group.members << user
+  module Admin
+    class MembersController < BaseController
+      def create
+        user = Forem.user_class.where(Forem.autocomplete_field => params[:user]).first
+        unless group.members.exists?(user.id)
+          group.members << user
+        end
+        render :status => :ok
       end
-      render :status => :ok
-    end
 
-    private
+      private
 
-    def group
-      @group ||= Forem::Group.find(params[:group_id])
+      def group
+        @group ||= Forem::Group.find(params[:group_id])
+      end
     end
   end
 end
