f114cf0a080e8579349a9be2a7094cf428b67883
Simon George <simon@sfcgeorge.co.uk>
2012-12-28 23:43:12 +0000

    Remove gemoji and replace it with emoticon_fontify gem.
    ## Removal of gemoji
    
    * Apple almost certainly doesn't freely licence their Emoji font so
    using it without permission is probably legally a Bad Idea™.
    * The Gemoji gem has issues disabled so if there's ever a problem
    asking questions or getting it fixed is near impossible.
    * Gemoji recomends serving the images from /public which won't work in
    a mounted engine.
    * Serving the Gemoji images from /assets is possible but slows down
    precompilation hugely (over 800 images!)
    * Do we really need over 800 emoji?
    
    ## Addition of emoticon_fontify
    
    * The emoticon font is properly licenced, free and open source.
    * Hundreds less emojis.
    * Instead of hard to remember codes like :stuck_out_tongue_closed_eyes:
    you can simply type an emoticon as you normally would and it is
    converted :-)
    * Helper method is built into the gem.
    * The gem is an engine so the necessary font and stylesheet assets are
    automatically included in the asset pipeline.
    * As it's a font the emoticons can easily be styled to match the
    forum's text color and size, it will zoom well, and display crisply on
    double pixel ratio devices.

--- app/helpers/forem/application_helper.rb
+++ app/helpers/forem/application_helper.rb
@@ -1,6 +1,9 @@
+require 'emoticon_fontify'
+
 module Forem
   module ApplicationHelper
     include FormattingHelper
+
     # processes text with installed markup formatter
     def forem_format(text, *options)
       forem_emojify(as_formatted_html(text))
@@ -34,20 +37,14 @@ module Forem
       end
     end
 
-    def forem_atom_auto_discovery_link_tag  
+    def forem_atom_auto_discovery_link_tag
       if controller_name == "topics" && action_name == "show"
         auto_discovery_link_tag(:atom)
       end
     end
 
     def forem_emojify(content)
-      h(content).to_str.gsub(/:([a-z0-9\+\-_]+):/) do |match|
-        if Emoji.names.include?($1)
-          '<img alt="' + $1 + '" height="20" src="' + asset_path("emoji/#{$1}.png") + '" style="vertical-align:middle" width="20" />'
-        else
-          match
-        end
-      end.html_safe if content.present?
+      ::EmoticonFontify.emoticon_fontify(h(content))
     end
   end
 end
