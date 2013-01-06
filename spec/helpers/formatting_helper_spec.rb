3b4c76bdc36cc0fb4477845132b4aadc4ac1a392
Phil Cohen <github@phlippers.net>
2013-01-05 17:07:04 -0800

    swap formatter engines based on ruby version:
    * This file had some weird whitespace and this
      diff is best viewed with the `-w` flag

--- spec/helpers/formatting_helper_spec.rb
+++ spec/helpers/formatting_helper_spec.rb
@@ -28,52 +28,59 @@ describe Forem::FormattingHelper do
   describe "as_quoted_text" do
     let(:raw_html) {"<p>html</p>"}
     describe "default formatter" do
-       before { Forem.formatter = nil }
+      before { Forem.formatter = nil }
 
-       describe "unsafe html" do
-         subject { helper.as_quoted_text(raw_html) }
-         it "is escaped" do
-           subject.should == "<blockquote>" + ERB::Util.h(raw_html) + "</blockquote>\n\n"
-         end
-         it {should be_html_safe}
-       end
+      describe "unsafe html" do
+        subject { helper.as_quoted_text(raw_html) }
+        it "is escaped" do
+          subject.should == "<blockquote>" + ERB::Util.h(raw_html) + "</blockquote>\n\n"
+        end
+        it {should be_html_safe}
+      end
 
-       describe "safe html" do
-         subject { helper.as_quoted_text(raw_html.html_safe) }
-         specify "is not escaped" do
-           subject.should == "<blockquote>" + raw_html + "</blockquote>\n\n"
-         end
-         it {should be_html_safe}
-       end
+      describe "safe html" do
+        subject { helper.as_quoted_text(raw_html.html_safe) }
+        specify "is not escaped" do
+          subject.should == "<blockquote>" + raw_html + "</blockquote>\n\n"
+        end
+        it {should be_html_safe}
+      end
     end
 
-    describe "Redcarpet" do
-       let(:markdown) { "**strong text**" }
-       before { Forem.formatter = Forem::Formatters::Redcarpet }
+    describe "Markdown" do
+      let(:markdown) { "**strong text**" }
+      before {
+        # MRI-specific C-extention tests
+        if RUBY_VERSION < "1.9" || RUBY_ENGINE == "ruby"
+          Forem.formatter = Forem::Formatters::Redcarpet
+        else
+          Forem.formatter = Forem::Formatters::Kramdown
+        end
+      }
 
-       describe "uses <blockquote> if no blockquote method" do
-         subject { helper.as_quoted_text(markdown) }
-         before { Forem.formatter.stub('respond_to?').with(:blockquote).and_return(false) }
-         it "wraps the content in blockquotes" do
-           subject.should == "<blockquote>#{markdown}</blockquote>\n\n"
-         end
-         it {should be_html_safe}
-       end
+      describe "uses <blockquote> if no blockquote method" do
+        subject { helper.as_quoted_text(markdown) }
+        before { Forem.formatter.stub('respond_to?').with(:blockquote).and_return(false) }
+        it "wraps the content in blockquotes" do
+          subject.should == "<blockquote>#{markdown}</blockquote>\n\n"
+        end
+        it {should be_html_safe}
+      end
 
       describe "uses formatter quoting method if exists" do
-         subject { helper.as_quoted_text(raw_html) }
-         before {
-           Forem.formatter.stub('respond_to?').with(:blockquote).and_return(true)
-           Forem.formatter.stub('respond_to?').with(:sanitize).and_return(false)
+        subject { helper.as_quoted_text(raw_html) }
+        before {
+          Forem.formatter.stub('respond_to?').with(:blockquote).and_return(true)
+          Forem.formatter.stub('respond_to?').with(:sanitize).and_return(false)
 
-           Forem.formatter.stub(:blockquote).and_return("> #{markdown}")
-         }
+          Forem.formatter.stub(:blockquote).and_return("> #{markdown}")
+        }
 
-         it "quotes the original content" do
-           subject.should == "> #{markdown}"
-         end
-         it {should be_html_safe}
-       end
+        it "quotes the original content" do
+          subject.should == "> #{markdown}"
+        end
+        it {should be_html_safe}
+      end
 
       describe "uses formatter sanitize method if exists" do
         subject { helper.as_formatted_html(markdown) }
