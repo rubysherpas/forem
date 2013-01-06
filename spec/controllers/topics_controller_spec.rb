84589df3c584f1fbb067dee39d2819d8e3e0fc81
Phil Cohen <github@phlippers.net>
2013-01-05 17:05:53 -0800

    controller behavior extracted from feature specs

--- spec/controllers/topics_controller_spec.rb
+++ spec/controllers/topics_controller_spec.rb
@@ -21,4 +21,18 @@ describe Forem::TopicsController do
       flash[:alert].should == "The topic you are looking for could not be found."
     end
   end
+
+
+  context "not signed in" do
+    let(:forum) { create(:forum) }
+    let(:user)  { create(:user, :login => 'other_forem_user', :email => "bob@boblaw.com", :custom_avatar_url => 'avatar.png') }
+    let(:topic) { create(:approved_topic, :forum => forum, :user => user) }
+
+    it "cannot delete topics" do
+      delete :destroy, :forum_id => topic.forum.to_param, :topic_id => topic.to_param, :id => topic.to_param
+      response.should redirect_to('/users/sign_in')
+      flash.alert.should == "You must sign in first."
+    end
+  end
+
 end
