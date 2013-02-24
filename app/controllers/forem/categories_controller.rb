dc4e0265b2046dd931f69deceb7574845945a2bc
Phil Cohen <github@phlippers.net>
2013-02-24 14:44:22 -0800

    eliminate some n+1 queries on categories#show

--- app/controllers/forem/categories_controller.rb
+++ app/controllers/forem/categories_controller.rb
@@ -1,6 +1,14 @@
 module Forem
   class CategoriesController < Forem::ApplicationController
+    before_filter :find_category, :only => :show
+
     helper 'forem/forums'
     load_and_authorize_resource :class => 'Forem::Category'
+
+    private
+
+    def find_category
+      @category = Forem::Category.includes(:forums => :category).find(params[:id])
+    end
   end
 end
