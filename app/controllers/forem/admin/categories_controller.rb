a75f7243412d7a0cca648222a980482f633edc27
Philip Arndt <parndt@gmail.com>
2013-05-10 13:34:02 +1200

    Refactored categories_controller into successful/failed callbacks.

--- app/controllers/forem/admin/categories_controller.rb
+++ app/controllers/forem/admin/categories_controller.rb
@@ -12,36 +12,55 @@ module Forem
       end
 
       def create
-        @category = Forem::Category.new(params[:category])
-        if @category.save
-          flash[:notice] = t("forem.admin.category.created")
-          redirect_to admin_categories_path
+        if @category = Forem::Category.create(params[:category])
+          create_successful
         else
-          flash.now.alert = t("forem.admin.category.not_created")
-          render :action => "new"
+          create_failed
         end
       end
 
       def update
         if @category.update_attributes(params[:category])
-          flash[:notice] = t("forem.admin.category.updated")
-          redirect_to admin_categories_path
+          update_successful
         else
-          flash.now.alert = t("forem.admin.category.not_updated")
-          render :action => "edit"
+          update_failed
         end
       end
 
       def destroy
         @category.destroy
+        destroy_successful
+      end
+
+      private
+      def find_category
+        @category = Forem::Category.find(params[:id])
+      end
+
+      def create_successful
+        flash[:notice] = t("forem.admin.category.created")
+        redirect_to admin_categories_path
+      end
+
+      def create_failed
+        flash.now.alert = t("forem.admin.category.not_created")
+        render :action => "new"
+      end
+
+      def destroy_successful
         flash[:notice] = t("forem.admin.category.deleted")
         redirect_to admin_categories_path
       end
 
-      private
-        def find_category
-          @category = Forem::Category.find(params[:id])
-        end
+      def update_successful
+        flash[:notice] = t("forem.admin.category.updated")
+        redirect_to admin_categories_path
+      end
+
+      def update_failed
+        flash.now.alert = t("forem.admin.category.not_updated")
+        render :action => "edit"
+      end
 
     end
   end
