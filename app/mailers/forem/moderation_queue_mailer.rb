e44e3cfca887fd630ef746515bb836436502e8f2
Ryan Bigg <radarlistener@gmail.com>
2013-06-03 07:22:39 -0700

    Reduce duplicate admin_users variable usage in moderation_queue_mailer

--- app/mailers/forem/moderation_queue_mailer.rb
+++ app/mailers/forem/moderation_queue_mailer.rb
@@ -4,16 +4,18 @@ module Forem
 
     def new_topic(topic)
       @topic = topic
-      
-      @admin_users = Forem.user_class.find_all_by_forem_admin(true)
-      mail(:to => @admin_users.map(&:email), :subject => I18n.t('forem.topic.moderation_needed'))
+      mail(:to => admin_users.map(&:email), :subject => I18n.t('forem.topic.moderation_needed'))
     end
 
     def new_post(post)
       @post = post
-      
-      @admin_users = Forem.user_class.find_all_by_forem_admin(true)
-      mail(:to => @admin_users.map(&:email), :subject => I18n.t('forem.post.moderation_needed'))
+      mail(:to => admin_users.map(&:email), :subject => I18n.t('forem.post.moderation_needed'))
+    end
+
+    private
+
+    def admin_users
+      Forem.user_class.find_all_by_forem_admin(true)
     end
   end
 end
