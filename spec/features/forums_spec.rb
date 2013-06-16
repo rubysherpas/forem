bf81b6137dfcf1d1d2d2f98adca930834253b4ca
Philip Arndt <p@arndt.io>
2013-06-16 23:55:56 +1200

    Minor speed improvements but big clarity improvements for forums_spec.
    Random spec order is handled with a support file so removed from .rspec

--- spec/features/forums_spec.rb
+++ spec/features/forums_spec.rb
@@ -15,18 +15,17 @@ describe "forums" do
 
   # Regression test for #352
   context "can include Markdown within a forum's description" do
-    before {
+    before(:all) do
       # MRI-specific C-extention tests
-      if Forem::Platform.mri?
-        Forem.formatter = Forem::Formatters::Redcarpet
+      Forem.formatter = if Forem::Platform.mri?
+        Forem::Formatters::Redcarpet
       else
-        Forem.formatter = Forem::Formatters::Kramdown
+        Forem::Formatters::Kramdown
       end
-    }
-    after { Forem.formatter = nil }
+    end
+    after(:all) { Forem.formatter = nil }
 
     it "includes a strong tag in a description" do
-
       forum.update_column(:description, "The **best** forum!")
       visit forums_path
       within(".forum") do
@@ -41,19 +40,19 @@ describe "forums" do
   end
 
   context "visiting a forum" do
+    let!(:topic_1) { FactoryGirl.create(:approved_topic, :subject => "Unpinned", :forum => forum) }
+    let!(:topic_2) { FactoryGirl.create(:approved_topic, :subject => "Most Recent", :forum => forum) }
+    let!(:topic_3) { FactoryGirl.create(:approved_topic, :subject => "PINNED!", :forum => forum, :pinned => true) }
+    let!(:topic_4) { FactoryGirl.create(:approved_topic, :subject => "HIDDEN!", :forum => forum, :hidden => true) }
     before do
-      @topic_1 = FactoryGirl.create(:approved_topic, :subject => "Unpinned", :forum => forum)
-      @topic_2 = FactoryGirl.create(:approved_topic, :subject => "Most Recent", :forum => forum)
-      @topic_2.update_attribute(:last_post_at, Time.now + 30.seconds)
-      @topic_3 = FactoryGirl.create(:approved_topic, :subject => "PINNED!", :forum => forum, :pinned => true)
-      @topic_4 = FactoryGirl.create(:approved_topic, :subject => "HIDDEN!", :forum => forum, :hidden => true)
+      topic_2.update_attribute(:last_post_at, Time.now + 30.seconds)
       visit forum_path(forum)
     end
 
     it "lists pinned topics first" do
       # TODO: cleaner way to get at topic subjects on the page?
-      topic_subjects = Nokogiri::HTML(page.body).css(".topics tbody tr .subject").map(&:text).join('\n')
-      topic_subjects.should include("PINNED!", "Most Recent", "Unpinned")
+      topic_subjects = Nokogiri::HTML(page.body).css(".topics tbody tr .subject").map{|s| s.text.strip}
+      topic_subjects.should == ["PINNED!", "Most Recent", "Unpinned"]
     end
 
     it "does not show hidden topics" do
@@ -63,14 +62,12 @@ describe "forums" do
     end
 
     context "when logged in" do
-      before do
-        @user = FactoryGirl.create(:user)
-        sign_in(@user)
-      end
+      let(:user) { FactoryGirl.create(:user) }
+      before { sign_in(user) }
 
       # Regression test for #272
       context "when forem_state is pending_review" do
-        let(:topic) { FactoryGirl.create(:topic, :forum => forum, :user => @user) }
+        let(:topic) { FactoryGirl.create(:topic, :forum => forum, :user => user) }
         let(:other_user) { FactoryGirl.create(:user, :login => "other_user") }
         let(:other_topic) { FactoryGirl.create(:topic, :subject => "topic started by other_user with forem_state pending_review", :forum => forum, :user => other_user) }
 
@@ -93,42 +90,35 @@ describe "forums" do
         end
 
         it "should show topic if logged in user is forem_admin" do
-          @user.forem_admin = true
+          user.forem_admin = true
           visit forum_path(forum)
           assert_seen("forem_user")
         end
       end
 
       it "calls out topics that have been posted to since your last visit, if you've visited" do
-        visit forum_topic_path(forum.id, @topic_2)
+        visit forum_topic_path(forum.id, topic_2)
         ::Forem::View.last.update_attribute(:updated_at, 1.minute.ago)
         visit forum_path(forum)
-        topic_subjects = Nokogiri::HTML(page.body).css(".topics tbody tr .new_posts")
-        topic_subjects.should_not be_empty
+        page.should have_css('.topics tbody tr .new_posts')
       end
 
       context "checking new topics" do
         before do
-          forum.register_view_by(@user)
-          forum.view_for(@user).update_attribute(:past_viewed_at, 3.days.ago)
-          @topic_1.created_at = 1.day.ago
-          @topic_2.created_at = 1.day.ago
-          @topic_3.created_at = 1.day.ago
-          @topic_4.created_at = 4.days.ago
+          forum.register_view_by(user)
+          forum.view_for(user).update_attribute(:past_viewed_at, 3.days.ago)
         end
 
         it "calls out new topics since last visit" do
           visit forum_path(forum)
-          new_topics = Nokogiri::HTML(page.body).css(".topics tbody tr super")
-          new_topics.size.should eq(3)
+          page.should have_css('.topics tbody tr super', :count => 3)
         end
 
         it "doesn't call out a topic that has been viewed" do
           visit forum_path(forum)
-          visit forum_topic_path(forum, @topic_1)
+          visit forum_topic_path(forum, topic_1)
           visit forum_path(forum)
-          new_topics = Nokogiri::HTML(page.body).css(".topics tbody tr super")
-          new_topics.size.should eq(2)
+          page.should have_css('.topics tbody tr super', :count => 2)
         end
       end
     end
