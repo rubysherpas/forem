adb0c589a4eb0d7994db54b51fbe928baa6f71e2
Ryan Bigg <radarlistener@gmail.com>
2013-09-18 09:59:23 +1000

    Remove references to Localeapp from Forem
    Too much trouble than it's worth
    
    Conflicts:
    	Gemfile

--- lib/forem/engine.rb
+++ lib/forem/engine.rb
@@ -19,16 +19,6 @@ module ::Forem
         forem/admin/members.js
       ]
     end
-
-    initializer "forem.localeapp", :before => :load_config_initializers do
-      require 'localeapp/rails'
-
-      Localeapp.configure do |config|
-        config.polling_environments = []
-        config.reloading_environments = []
-        config.sending_environments = []
-      end
-    end
   end
 end
 
