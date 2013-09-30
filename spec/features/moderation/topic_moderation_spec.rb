d2dde7ee279a87af933012ddb5d9325cfffecd9c
Steve Morss <smorss@catalinacomputing.com>
2013-09-30 14:11:52 -0400

    Created tests for topic moderation view.

--- spec/features/moderation/topic_moderation_spec.rb
+++ spec/features/moderation/topic_moderation_spec.rb
@@ -17,6 +17,33 @@ describe "moderation" do
       forum.moderators << group
       sign_in(moderator)
     end
+    
+    context "moderation tools view" do
+      before do
+        visit forum_path(forum)
+        click_link "Moderation Tools"
+      end
+      
+      it "should have no posts" do
+        page.should have_content("No posts")        
+      end
+      
+      it "should have 1 topic" do
+        page.should have_content("1 topic")
+        within("#topics") do
+          assert find("#topic_1")
+          assert find(".topic")
+          assert find(".odd")
+        end
+      end
+
+      it "should have a link to the topic moderation page" do
+        assert find_link("FIRST TOPIC")
+        click_link("FIRST TOPIC")
+        assert page.current_path.should match(forum_topic_path(forum, topic))
+      end
+
+    end
 
     context "singular moderation" do
       it "can approve a topic by a new user" do
