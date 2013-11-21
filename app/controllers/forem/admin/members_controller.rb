d12b99576cf4e194f205852b755da42c10203cbe
bodrovis <twillight_church@bk.ru>
2013-11-21 14:01:23 +0400

    Ability to remove users from groups
    Fixes #503

--- app/controllers/forem/admin/members_controller.rb
+++ app/controllers/forem/admin/members_controller.rb
@@ -9,6 +9,17 @@ module Forem
         redirect_to [:admin, group]
       end
 
+      def destroy
+        user = Forem.user_class.where(:id => params[:id]).first
+          if group.members.exists?(user.id)
+            group.members.delete(user)
+            flash[:notice] = t("forem.admin.groups.show.member_removed")
+          else
+            flash[:alert] = t("forem.admin.groups.show.no_member_to_remove")
+          end
+        redirect_to [:admin, group]
+      end
+
       private
 
       def group
