a70f6648074748269f6c0929bf388e31e91b00f7
Ryan Bigg <radarlistener@gmail.com>
2014-01-13 07:16:45 +1100

    Forum routes should be matched last so that they do not interfere with admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,9 @@
 Forem::Engine.routes.draw do
   root :to => "forums#index"
 
-  resources :forums, :only => [:index, :show], :path => "/" do
-    resources :topics do
-      member do
-        get :subscribe
-        get :unsubscribe
-      end
-    end
-  end
-
   resources :topics, :only => [:new, :create, :index, :show, :destroy] do
     resources :posts
   end
-
-  get 'forums/:forum_id/moderation', :to => "moderation#index", :as => :forum_moderator_tools
-  # For mass moderation of posts
-  put 'forums/:forum_id/moderate/posts', :to => "moderation#posts", :as => :forum_moderate_posts
-  # Moderation of a single topic
-  put 'forums/:forum_id/topics/:topic_id/moderate', :to => "moderation#topic", :as => :moderate_forum_topic
 
   resources :categories, :only => [:index, :show]
 
@@ -46,5 +31,20 @@ Forem::Engine.routes.draw do
     end
 
     get 'users/autocomplete', :to => "users#autocomplete", :as => "user_autocomplete"
+  end
+
+  get '/:forum_id/moderation', :to => "moderation#index", :as => :forum_moderator_tools
+  # For mass moderation of posts
+  put '/:forum_id/moderate/posts', :to => "moderation#posts", :as => :forum_moderate_posts
+  # Moderation of a single topic
+  put '/:forum_id/topics/:topic_id/moderate', :to => "moderation#topic", :as => :moderate_forum_topic
+
+  resources :forums, :only => [:index, :show], :path => "/" do
+    resources :topics do
+      member do
+        get :subscribe
+        get :unsubscribe
+      end
+    end
   end
 end
