f937c385bae416e99f5994153381d4d809e58ddf
Philippe Durix <philippe.durix@me.com>
2014-02-20 04:28:57 +0100

    Fixed a regression introduced by commit #54c57330c419a597078170143398b33427114ae0
    Conflicts:
    	app/models/forem/topic.rb

--- app/models/forem/topic.rb
+++ app/models/forem/topic.rb
@@ -56,7 +56,7 @@ module Forem
       end
 
       def by_pinned_or_most_recent_post
-        order('forem_topics.pinned DESC').
+	      order('forem_topics.pinned DESC').
         order('forem_topics.last_post_at DESC').
         order('forem_topics.id')
       end
