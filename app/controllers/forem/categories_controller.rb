0e7d123060c259021374d241496efbf891065dc5
Ryan Bigg <radarlistener@gmail.com>
2014-03-05 09:42:42 -0500

    Scope Forem::CategoriesController correctly

--- app/controllers/forem/categories_controller.rb
+++ app/controllers/forem/categories_controller.rb
@@ -1,6 +1,13 @@
 module Forem
   class CategoriesController < Forem::ApplicationController
     helper 'forem/forums'
+    before_filter :find_category 
     load_and_authorize_resource :class => 'Forem::Category'
+
+    private
+
+    def find_category
+      @category = Forem::Category.scoped_to(current_account).find(params[:id])
+    end
   end
 end
