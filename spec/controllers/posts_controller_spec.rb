50bbbe1c9ca9cd06950a7f7633afbdce88f1adad
Alex Watt <alexcwatt@gmail.com>
2014-05-15 18:09:08 -0400

    Redirect posts#show to topics#show
    Fixes #576

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -12,6 +12,11 @@ describe Forem::PostsController do
       response.should redirect_to('/users/sign_in')
       flash.alert.should == "You must sign in first."
     end
+
+    it "can be redirected to post on topic" do
+      get :show, :topic_id => topic.to_param, :id => first_post.to_param
+      response.should redirect_to("/forem/welcome-to-forem/topics/first-topic?page=1#post-#{first_post.id}")
+    end
   end
 
   context 'signed in' do
@@ -53,7 +58,7 @@ describe Forem::PostsController do
         end
 
         it "cannot access the new action" do
-          get :new, 
+          get :new,
             :forum_id => forum.to_param,
             :topic_id => topic.to_param
           flash[:alert].should == 'You are not allowed to do that.'
@@ -86,17 +91,17 @@ describe Forem::PostsController do
         end
       end
     end
-    
+
     context 'when attempting to destroy posts' do
       it 'can with permission' do
         delete :destroy, :topic_id => topic, :id => topic.posts.first
         flash[:notice].should == "Only post in topic deleted. Topic also deleted."
       end
-      
+
       it 'cannot without permission' do
         # remove destroy permission
         controller.current_user.stub :can_destroy_forem_posts? => false
-        
+
         delete :destroy, :topic_id => topic, :id => topic.posts.first
         flash[:alert].should == 'You are not allowed to do that.'
         response.should redirect_to(root_path)
