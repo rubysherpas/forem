1ce2d83996f32861d47c2ccc09c996a287a547d1
Kirill Shirinkin <fodojyko@gmail.com>
2014-08-30 20:46:00 +0200

    newer emoji

--- app/helpers/forem/application_helper.rb
+++ app/helpers/forem/application_helper.rb
@@ -34,16 +34,16 @@ module Forem
       end
     end
 
-    def forem_atom_auto_discovery_link_tag  
+    def forem_atom_auto_discovery_link_tag
       if controller_name == "topics" && action_name == "show"
         auto_discovery_link_tag(:atom)
       end
     end
 
     def forem_emojify(content)
-      h(content).to_str.gsub(/:([a-z0-9\+\-_]+):/) do |match|
-        if Emoji.names.include?($1)
-          '<img alt="' + $1 + '" height="20" src="' + asset_path("emoji/#{$1}.png") + '" style="vertical-align:middle" width="20" />'
+      h(content).to_str.gsub(/:([\w+-]+):/) do |match|
+        if emoji = Emoji.find_by_alias($1)
+          %(<img alt="#$1" src="#{asset_path("emoji/#{emoji.image_filename}", type: :image)}" style="vertical-align:middle" width="20" height="20" />)
         else
           match
         end
