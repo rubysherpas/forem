f0520c1d9514244528fa71a85672c750065d61b9
Erik Ostrom <erik@echographia.com>
2015-07-07 14:49:39 -0500

    Include topics in /forum/:id API.

--- app/views/forem/api/v1/forums/show.json.jbuilder
+++ app/views/forem/api/v1/forums/show.json.jbuilder
@@ -5,3 +5,20 @@ json.data do
     json.(@forum, :title, :slug)
   end
 end
+
+json.relationships do
+  json.topics do
+    json.data @forum.topics do |topic|
+      json.type 'topics'
+      json.(topic, :id)
+    end
+  end
+end
+
+json.included @forum.topics do |topic|
+  json.type 'topics'
+  json.(topic, :id)
+  json.attributes do
+    json.(topic, :subject)
+  end
+end
--- spec/requests/api/forums_api_spec.rb
+++ spec/requests/api/forums_api_spec.rb
@@ -2,6 +2,7 @@ require 'spec_helper'
 
 describe 'Forums API', type: :request do
   let(:forum) { create(:forum) }
+  let!(:topic) { create(:topic, forum: forum) }
 
   def api(method, action, params = {})
     send method, action, params,
@@ -23,6 +24,26 @@ describe 'Forums API', type: :request do
       expect(data[:id]).to eq forum.id
       expect(data[:attributes][:title]).to eq forum.title
       expect(data[:attributes][:slug]).to eq forum.slug
+    end
+
+    let(:related_topics) { json[:relationships][:topics] }
+    let(:included_objects) { json[:included] }
+
+    it 'describes topic relationships' do
+      expect(related_topics[:data].length).to eq 1
+      related_topic = related_topics[:data].first
+
+      expect(related_topic[:type]).to eq 'topics'
+      expect(related_topic[:id]).to eq topic.id
+    end
+
+    it 'includes topic data' do
+      expect(included_objects.length).to eq 1
+      included_topic = included_objects.first
+
+      expect(included_topic[:type]).to eq 'topics'
+      expect(included_topic[:id]).to eq topic.id
+      expect(included_topic[:attributes][:subject]).to eq topic.subject
     end
   end
 end
