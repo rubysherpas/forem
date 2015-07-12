856b53ecf079c0d5cf2808959f49a0c15d1344cb
Ryan Bigg <git@ryanbigg.com>
2015-07-12 20:56:50 +1000

    Remove commented-out routes from config/routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,6 @@
 Forem::Engine.routes.draw do
   root :to => "forums#index"
 
-  # resources :topics, :only => [:new, :create, :index, :show, :destroy] do
-  #   resources :posts
-  # end
-
   resources :categories, :only => [:index, :show]
 
   namespace :admin do
