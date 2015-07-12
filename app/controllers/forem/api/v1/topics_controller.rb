aa614d91e624f8761f734dbc3fe5aad47f7fce4e
Erik Ostrom <erik@echographia.com>
2015-07-12 15:07:13 -0500

    Create Topic error handling.
    Tried to cover all of the MUSTs and some of the SHOULDs from the JSON
    API spec.
    
    Specific errors are just returned as text messages for now.
    
    Most of the new controller code will be extracted to a mixin module.
    Likewise, the spec code can be extracted to shared behaviors.

--- app/controllers/forem/api/v1/topics_controller.rb
+++ app/controllers/forem/api/v1/topics_controller.rb
@@ -6,7 +6,16 @@ module Forem
       class TopicsController < Forem::TopicsController
         before_action { request.format = :json }
 
+        before_filter :client_generated_ids_are_unsupported
+
+        rescue_from ActionController::ParameterMissing, with: :bad_request
+        rescue_from CanCan::AccessDenied, with: :forbidden
+
         protected
+
+        def authenticate_forem_user
+          render nothing: true, status: :forbidden if !forem_user
+        end
 
         def topic_params
           params.require(:data).require(:attributes).permit(:subject, :posts_attributes => [[:text]])
@@ -17,7 +26,19 @@ module Forem
         end
 
         def create_unsuccessful
-          raise 'TODO'
+          render 'member_errors', status: :bad_request
+        end
+
+        def bad_request
+          render nothing: true, status: :bad_request
+        end
+
+        def forbidden
+          render nothing: true, status: :forbidden
+        end
+
+        def client_generated_ids_are_unsupported
+          render nothing: true, status: :forbidden if params[:data][:id]
         end
       end
     end
--- spec/requests/api/topics_api_spec.rb
+++ spec/requests/api/topics_api_spec.rb
@@ -7,14 +7,14 @@ describe 'Topics API', type: :request do
   before { sign_in user }
 
   describe '#create' do
+    let(:input_data) { {type: 'topics', attributes: attributes} }
+    let(:attributes) { {subject: 'New topic'} }
+    let(:authorized?) { true }
+
     before do
-      api :post, api_forum_topics_path(forum),
-        data: {
-          type: 'topics',
-          attributes: {
-            subject: 'New topic'
-          }
-        }
+      Forem::Ability.any_instance.stub(:cannot?) { true } if !authorized?
+
+      api :post, api_forum_topics_path(forum), data: input_data
     end
 
     let(:json) { JSON.parse(response.body).with_indifferent_access }
@@ -34,6 +34,88 @@ describe 'Topics API', type: :request do
       expect(data[:attributes][:subject]).to eq 'New topic'
       expect(data[:attributes][:views_count]).to eq 0
       expect(data[:attributes][:posts_count]).to eq 0
+    end
+
+    let(:errors) { json[:errors] }
+
+    describe 'with valid data' do
+      it 'succeeds' do
+        expect(response.response_code).
+          to eq Rack::Utils::status_code(:created)
+      end
+
+      it 'provides a URL for the new topic' do
+        expect(URI.parse(response.location).path).
+          to eq api_forum_topic_path(forum, Forem::Topic.last)
+      end
+
+      it 'responds with JSON for the new topic' do
+        expect(data[:type]).to eq 'topics'
+        expect(data[:attributes][:subject]).to eq 'New topic'
+        expect(data[:attributes][:views_count]).to eq 0
+        expect(data[:attributes][:posts_count]).to eq 0
+      end
+    end
+
+    describe 'with no data' do
+      let(:attributes) { {} }
+
+      it 'fails' do
+        expect(response.response_code).
+          to eq Rack::Utils::status_code(:bad_request)
+      end
+    end
+
+    describe 'with invalid data' do
+      let(:attributes) { {nonexistent_attribute: 'New topic'} }
+
+      it 'fails' do
+        expect(response.response_code).
+          to eq Rack::Utils::status_code(:bad_request)
+      end
+
+      it 'does not create a topic' do
+        expect(Forem::Topic.count).to eq 0
+      end
+
+      it 'returns error objects' do
+        expect(errors).not_to be_blank
+
+        expect(errors.first[:title]).to eq "Subject can't be blank"
+      end
+    end
+
+    describe 'with a client-generated ID' do
+      let(:input_data) {
+        {type: 'topics', id: 'generated-ID', attributes: attributes}
+      }
+
+      it 'is forbidden' do
+        expect(response.response_code).
+          to eq Rack::Utils::status_code(:forbidden)
+      end
+    end
+
+    describe 'from an unauthenticated user' do
+      let(:user) { nil }
+
+      it 'is forbidden' do
+        expect(response.response_code).
+          to eq Rack::Utils::status_code(:forbidden)
+      end
+
+      it 'does not create a topic' do
+        expect(Forem::Topic.count).to eq 0
+      end
+    end
+
+    describe 'from an unauthorized user' do
+      let(:authorized?) { false }
+
+      it 'is forbidden' do
+        expect(response.response_code).
+          to eq Rack::Utils::status_code(:forbidden)
+      end
     end
   end
 end
