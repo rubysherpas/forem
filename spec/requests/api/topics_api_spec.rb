a60d5d5f2c5221628032bbf0b4acd827cbbedebd
Erik Ostrom <erik@echographia.com>
2015-07-15 13:39:36 -0500

    Removed accidentally duplicated specs.

--- spec/requests/api/topics_api_spec.rb
+++ spec/requests/api/topics_api_spec.rb
@@ -22,22 +22,6 @@ describe 'Topics API', type: :request do
 
     before { api :post, api_forum_topics_path(forum), data: input_data }
 
-    it 'succeeds' do
-      expect(response.response_code).to eq Rack::Utils::status_code(:created)
-    end
-
-    it 'provides a URL for the new topic' do
-      expect(URI.parse(response.location).path).
-        to eq api_forum_topic_path(forum, Forem::Topic.last)
-    end
-
-    it 'responds with JSON for the new topic' do
-      expect(data[:type]).to eq 'topics'
-      expect(data[:attributes][:subject]).to eq 'New topic'
-      expect(data[:attributes][:views_count]).to eq 0
-      expect(data[:attributes][:posts_count]).to eq 0
-    end
-
     describe 'with valid data' do
       it 'succeeds' do
         expect(response.response_code).
