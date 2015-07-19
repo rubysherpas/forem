b2684a80f307f3d3fcf2c82f8cc71ec757010c1f
Erik Ostrom <erik@echographia.com>
2015-07-19 00:38:06 -0500

    API: Inherit from ForumsController.
    This is the pattern we're using for the other controllers, to get
    permissions checking and such.
    
    Also removed unnecessary `respond_to` from `ForumsController#show`,
    and wrote a test to confirm that it was unnecessary.

--- app/controllers/forem/api/v1/forums_controller.rb
+++ app/controllers/forem/api/v1/forums_controller.rb
@@ -3,9 +3,7 @@ require_dependency "forem/application_controller"
 module Forem
   module Api
     module V1
-      class ForumsController < ApplicationController
-        load_and_authorize_resource :class => 'Forem::Forum', :only => :show
-
+      class ForumsController < Forem::ForumsController
         rescue_from CanCan::AccessDenied, with: :forbidden
 
         before_action { request.format = :json }
